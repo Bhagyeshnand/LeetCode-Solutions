64c411d2e8c7ad572549e5a12ea6ad0d129ecb93
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-06-03 06:16:37 +0530

    Time: 2 ms (100%), Space: 45.3 MB (50.79%) - LeetHub

--- 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.java
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.java
@@ -1,14 +1,21 @@
-public class Solution {
+class Solution {
     public int appendCharacters(String s, String t) {
-        int i = 0, j = 0;  // Start both pointers at the beginning of s and t
-        
-        while (i < s.length() && j < t.length()) {  // Continue until one of the strings is fully scanned
-            if (s.charAt(i) == t.charAt(j)) {  // If characters match
-                j++;  // Move the pointer in t forward
-            }
-            i++;  // Always move the pointer in s forward
+        if(s.equals(t))
+            return 0;
+        char chS[] = s.toCharArray();
+        char chT[] = t.toCharArray();
+
+        int i = 0, j = 0;
+        int m = chS.length, n = chT.length;
+        for(; i < m; i++)
+        {
+            if(chS[i] == chT[j])
+                j++;
+            
+            if(j == n)
+                return 0;
         }
+        return n - j;
         
-        return t.length() - j;  // The number of characters in t not matched in s
     }
 }
