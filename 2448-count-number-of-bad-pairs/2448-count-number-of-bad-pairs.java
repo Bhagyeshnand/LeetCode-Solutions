5ce62fb7fcb7f4ebfce952dfabf5ecb6acf7ed8a
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-06-09 22:58:56 +0530

    Update 2448-count-number-of-bad-pairs.java

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -1,3 +1,19 @@
+/* 
+Count Good pairs instead of Bad pairs
+Ans = Total pairs - No of Good pairs
+Number of total pairs where i<j = (n*(n-1))/2
+
+A pair is said to be good if i < j and j - i == nums[j] - nums[i]
+This can be rewritten as j - nums[j] == i- nums[i]
+
+Keep a track of this difference using a map
+
+If difference is found in future -
+It is a good pair, add its count to goodpair_count
+increment its count also in the map as well.
+*/
+
+
 class Solution {
     public long countBadPairs(int[] nums) {
          long goodPairs = 0;
