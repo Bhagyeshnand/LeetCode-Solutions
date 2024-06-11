231b3770f767d439aadacd110f5d868607fd8dee
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-06-11 08:10:50 +0530

    Time: 0 ms (100%), Space: 42 MB (61.6%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int max = 1000;
+        for( int n : arr1) max = Math.max(max, n);
+
+        int freq[] = new int[max+1];
+        for(int n : arr1) freq[n]++;
+
+        int i = 0,j = 0;
+        int[] ans = new int[arr1.length];
+        for(i = 0; i< arr2.length; i++){
+            while(freq[arr2[i]] > 0){
+                ans[j++] = arr2[i];
+                freq[arr2[i]]--;
+            }
+        }
+    
+        for(int k = 0; k< freq.length; k++){
+            while(freq[k] > 0){
+                ans[j++] = k;
+                freq[k]--;
+            }
+        }
+        return ans;
+        
+    }
+}
