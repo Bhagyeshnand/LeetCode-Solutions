618adc54fa1580a5864dd16f59cdb916f8ee1655
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-06-14 21:25:19 +0530

    Time: 1 ms (99.57%), Space: 41.5 MB (66.33%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
@@ -1,17 +1,34 @@
 class Solution {
-
-    public boolean isVowel(char ch) {
-        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' );
-    }
     public boolean halvesAreAlike(String s) {
-        int n =s.length();
-        int i = 0, j = n/2, countleft = 0, countright = 0;
+        int[] a = new int[123];
+        int[] b = new int[123];
+        int strLen = s.length();
+        return checkVowel(0, strLen/2, a, s) == checkVowel(strLen/2, strLen, b, s);
+    }
 
-        while(i<n/2 && j<n){
-            if(isVowel(s.charAt(i++))) ++countleft;
-            if(isVowel(s.charAt(j++))) ++countright;
+    public static int checkVowel(int start, int end, int[] freq, String s){
+        while(start < end) {
+            freq[s.charAt(start++)]++;
         }
-        return countleft == countright;
+        return freq['a'] + freq['A'] + freq['e']+freq['E']+freq['i']+freq['I']+freq['o']+freq['O']+
+                freq['u']+freq['U'];
     }
 }
+
+// class Solution {
+
+//     public boolean isVowel(char ch) {
+//         return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+//                 ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' );
+//     }
+//     public boolean halvesAreAlike(String s) {
+//         int n =s.length();
+//         int i = 0, j = n/2, countleft = 0, countright = 0;
+
+//         while(i<n/2 && j<n){
+//             if(isVowel(s.charAt(i++))) ++countleft;
+//             if(isVowel(s.charAt(j++))) ++countright;
+//         }
+//         return countleft == countright;
+//     }
+// }
