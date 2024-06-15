c722c34b4a40c105792c54ba6c1487680cfbec12
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-06-15 23:03:07 +0530

    Time: 2 ms (100%), Space: 47.1 MB (96.53%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -1,5 +1,5 @@
-class Solution {
-        public int equalPairs(int[][] grid) {
+/*class Solution {
+    public int equalPairs(int[][] grid) {
         Map<String, Integer> cnt = new HashMap<>();
         for (int[] row : grid) {
             cnt.merge(Arrays.toString(row), 1, Integer::sum);
@@ -13,5 +13,50 @@ class Solution {
             pairs += cnt.getOrDefault(Arrays.toString(col), 0);
         }
         return pairs;
+    }
+}
+
+/ cnt.merge(Arrays.toString(row), 1, Integer::sum); is same as
+
+// String key = Arrays.toString(row);
+// cnt.put(key, 1 + cnt.getOrDefault(key, 0));
+// Arrays.toString(row)/value is key/value binding of HashMap cnt;
+
+// Integer::sum here means that the method sum of Integer 
+//  is used to increase by 1
+//  the value corresponding to key: Arrays.toString(row).
+//  The default value is 0 in case the key is absent.
+
+
+*/
+class Solution {
+    public int equalPairs(int[][] grid) {
+        int ans = 0;
+        int n = grid.length;
+        Map<Integer,Integer> map = new HashMap();
+        for(int i=0;i<n;i++){
+            int hash = rowHash(grid[i]);
+            map.put(hash,map.getOrDefault(hash,0)+1);
+        }
+        for(int i=0;i<n;i++){
+            int hash = colHash(grid, i);
+            ans+=map.getOrDefault(hash,0);
+        }
+        return ans;
+    }
+    private int rowHash(int[] arr){
+        int hash = 0;
+        for(int i:arr){
+            hash=i+7*hash;
+        }
+        return hash;
+    }
+    private int colHash(int[][] grid, int i){
+        int n = grid.length;
+        int hash = 0;
+        for(int[] x: grid){
+            hash = x[i]+7*hash;
+        }
+        return hash;
     }
 }
