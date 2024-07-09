cf1e49959baa4424477ea6beebcb89732430a777
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-07-09 11:22:44 +0530

    Time: 3 ms (93.06%), Space: 72.6 MB (73.06%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.java
+++ 1803-average-waiting-time/1803-average-waiting-time.java
@@ -1,19 +1,32 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        double total_waiting_time = 0;
-        int current_time = 0;
-
-        for (int[] customer : customers) {
-            int arrival = customer[0];
-            int service = customer[1];
-            if (current_time < arrival) {
-                current_time = arrival;
-            }
-            int waiting_time = current_time - arrival + service;
-            total_waiting_time += waiting_time;
-            current_time += service;
+        double wait = 0;
+        int t = -1;
+        for(int[] a : customers) {
+            t = (t<a[0])?a[0]:t;
+            t += a[1];
+            wait += t-a[0];
         }
-
-        return total_waiting_time / customers.length;
+        return wait/customers.length;
     }
 }
+
+// class Solution {
+//     public double averageWaitingTime(int[][] customers) {
+//         double total_waiting_time = 0;
+//         int current_time = 0;
+
+//         for (int[] customer : customers) {
+//             int arrival = customer[0];
+//             int service = customer[1];
+//             if (current_time < arrival) {
+//                 current_time = arrival;
+//             }
+//             int waiting_time = current_time - arrival + service;
+//             total_waiting_time += waiting_time;
+//             current_time += service;
+//         }
+
+//         return total_waiting_time / customers.length;
+//     }
+// }
