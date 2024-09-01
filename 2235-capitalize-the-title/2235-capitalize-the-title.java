de3bc5d116675e2811d92698e163570dc82068f9
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-09-01 23:56:27 +0530

    Time: 1 ms (98.32%), Space: 42 MB (87.44%) - LeetHub

--- 2235-capitalize-the-title/2235-capitalize-the-title.java
+++ 2235-capitalize-the-title/2235-capitalize-the-title.java
@@ -1,18 +1,42 @@
 class Solution {
     public String capitalizeTitle(String title) {
-        String[] arr = title.split(" ");
-        String ans = "";
-        
-        for (String s : arr) {
-            int n = s.length();
-            if (n == 1 || n == 2) {
-                ans += s.toLowerCase() + " ";
-            } else {
-                s = s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
-                ans += s + " ";
+        title = title.toLowerCase();
+        int diff = 'a'-'A';
+        int length = 0;
+        char[] arr = title.toCharArray();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==' '){
+                if(length>2){
+                    arr[i-length] -= diff;
+                }
+                length = 0;
+            }
+            else{
+                length++;
             }
         }
-        
-        return ans.trim();
+        if(length>2){
+            arr[arr.length-length] -= diff;
+        }
+        return new String(arr);
     }
 }
+
+// class Solution {
+//     public String capitalizeTitle(String title) {
+//         String[] arr = title.split(" ");
+//         String ans = "";
+        
+//         for (String s : arr) {
+//             int n = s.length();
+//             if (n == 1 || n == 2) {
+//                 ans += s.toLowerCase() + " ";
+//             } else {
+//                 s = s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+//                 ans += s + " ";
+//             }
+//         }
+        
+//         return ans.trim();
+//     }
+// }
