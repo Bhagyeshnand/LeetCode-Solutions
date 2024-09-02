6b2bee7ae3069aa7de69babf5e13e39e9ba3e039
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-09-03 00:44:12 +0530

    Time: 1 ms (53.29%), Space: 41.3 MB (76.16%) - LeetHub

---
+++ 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.java
@@ -0,0 +1,13 @@
+class Solution {
+        public int minTimeToType(String word) {
+        int cnt = word.length();
+        char prev = 'a';
+        for (int i = 0; i < word.length(); ++i) {
+            char cur = word.charAt(i);
+            int diff = Math.abs(cur - prev);
+            cnt += Math.min(diff, 26 - diff);
+            prev = cur;
+        }
+        return cnt;
+    }
+}
