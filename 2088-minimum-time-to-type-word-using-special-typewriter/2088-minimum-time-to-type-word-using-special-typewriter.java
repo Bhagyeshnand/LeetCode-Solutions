81a06c27cb65a49b5bd9ab9beeb586fe71a5e35b
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-09-03 00:49:12 +0530

    Time: 0 ms (100%), Space: 41.2 MB (89.34%) - LeetHub

--- 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.java
+++ 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.java
@@ -1,12 +1,12 @@
 class Solution {
         public int minTimeToType(String word) {
-        int cnt = word.length();
+        int cnt = word.length(); // for typing cost
         char prev = 'a';
         for (int i = 0; i < word.length(); ++i) {
-            char cur = word.charAt(i);
-            int diff = Math.abs(cur - prev);
-            cnt += Math.min(diff, 26 - diff);
-            prev = cur;
+            char cur = word.charAt(i);          // current
+            int diff = Math.abs(cur - prev);    // prev and curr difference
+            cnt += Math.min(diff, 26 - diff);   // go on those side which is min
+            prev = cur;                         // updating prev pointer
         }
         return cnt;
     }
