6933a7062a6e463cee79583ca2dcb1b9b431a72e
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-09-12 10:29:05 +0530

    Time: 4 ms (100%), Space: 45.3 MB (52.29%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.java
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.java
@@ -1,22 +1,44 @@
 class Solution {
+    boolean[] s;
     public int countConsistentStrings(String allowed, String[] words) {
-        int count = 0;
-        HashSet <Character> st = new HashSet<>();
-
-        for(char e : allowed.toCharArray()){
-            st.add(e);
+        s = new boolean[26];
+        for (char c : allowed.toCharArray()) {
+            s[c - 'a'] = true;
         }
-
-        for( String s : words){
-            count++;
-            for( char ch : s.toCharArray()){
-                if(!st.contains(ch)){
-                    count--;
-                    break;
-                }
-            }
+        int ans = 0;
+        for (String w : words) {
+            if (check(w)) ++ans;
         }
+        return ans;
+    }
 
-        return count;
+    private boolean check(String w) {
+        for (int i = 0; i < w.length(); ++i) {
+            if (!s[w.charAt(i) - 'a']) return false;
+        }
+        return true;
     }
 }
+
+// class Solution {
+//     public int countConsistentStrings(String allowed, String[] words) {
+//         int count = 0;
+//         HashSet <Character> st = new HashSet<>();
+
+//         for(char e : allowed.toCharArray()){
+//             st.add(e);
+//         }
+
+//         for( String s : words){
+//             count++;
+//             for( char ch : s.toCharArray()){
+//                 if(!st.contains(ch)){
+//                     count--;
+//                     break;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+// }
