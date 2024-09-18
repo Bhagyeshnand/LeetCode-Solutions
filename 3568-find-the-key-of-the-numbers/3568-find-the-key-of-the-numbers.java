cb6dc56d75af1878ddfb6082a94b7c00eec17dd0
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-09-18 18:19:23 +0530

    Time: 1 ms (98.9%), Space: 41.1 MB (75.74%) - LeetHub

--- 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.java
+++ 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.java
@@ -1,29 +1,15 @@
 class Solution {
     public int generateKey(int num1, int num2, int num3) {
-        int sum=0,c=0,co=0;
-
-        while(c<4){
-            int a = num1%10, b=num2%10, d=num3%10;
-            int e = Math.min(a,Math.min(b,d));
-            if(sum==0 && e==0){
-                co++;
-            }
-            sum = (sum*10) + e;
-            num1/=10;
-            num2/=10;
-            num3/=10;
-            c++;
+        int ans = 0, p = 1;
+        for (int i = 0; i < 4; i++) {
+            int t = Math.min(num1 % 10, num2 % 10);
+            t = Math.min(t, num3 % 10);
+            ans += t * p;
+            num1 /= 10;
+            num2 /= 10;
+            num3 /= 10;
+            p *= 10;
         }
-        int num=0;
-        while(sum!=0){
-            int a = sum%10;
-            num = (num*10)+a;
-            sum/=10;
-        }
-        while(co!=0){
-            num*=10;
-            co--;
-        }
-        return num;
+        return ans;
     }
 }
