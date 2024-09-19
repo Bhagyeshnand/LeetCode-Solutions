d80e19e379ecdabd2019a6664a79bf5b11373b44
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-09-19 19:29:31 +0530

    Time: 2 ms (68.85%), Space: 42.5 MB (52.36%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,18 +1,24 @@
-class Solution {
+public class Solution{
+// Helper function to solve the expression in different ways
     public List<Integer> solve(String s) {
         List<Integer> result = new ArrayList<>();
 
+        // Iterate through each character in the string
         for (int i = 0; i < s.length(); i++) {
-            char curr = s.charAt(i);
-            if (curr == '+' || curr == '-' || curr == '*') {
-                List<Integer> left = solve(s.substring(0, i));
-                List<Integer> right = solve(s.substring(i + 1));
+            char c = s.charAt(i);
 
-                for (int x : left) {
-                    for (int y : right) {
-                        if (curr == '+') {
+            // If the current character is an operator
+            if (c == '+' || c == '-' || c == '*') {
+                // Recursively solve left and right substrings
+                List<Integer> leftResults = solve(s.substring(0, i));
+                List<Integer> rightResults = solve(s.substring(i + 1));
+
+                // Combine the results from left and right parts
+                for (int x : leftResults) {
+                    for (int y : rightResults) {
+                        if (c == '+') {
                             result.add(x + y);
-                        } else if (curr == '-') {
+                        } else if (c == '-') {
                             result.add(x - y);
                         } else {
                             result.add(x * y);
@@ -21,13 +27,17 @@ class Solution {
                 }
             }
         }
-        if (result.size() == 0) {
+
+        // If no operator is found, it means the string is a number
+        if (result.isEmpty()) {
             result.add(Integer.parseInt(s));
         }
+
         return result;
     }
 
-    public List<Integer> diffWaysToCompute(String s) {
-        return solve(s);
+    // Main function to compute different ways to compute the expression
+    public List<Integer> diffWaysToCompute(String expression) {
+        return solve(expression);
     }
 }
