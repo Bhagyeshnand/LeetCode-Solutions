91c99b2588a51a5d02f8e238cdefbabe70b65f6f
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-09-21 23:29:42 +0530

    Time: 1 ms (100%), Space: 43.2 MB (92.75%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,20 +1,35 @@
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         HashMap<Character, Integer> charIndexMap = new HashMap<>();
+//         int maxLength = 0;
+//         int left = 0;
+
+//         for (int right = 0; right < s.length(); right++) {
+//             char currentChar = s.charAt(right);
+//             if (charIndexMap.containsKey(currentChar) && charIndexMap.get(currentChar) >= left) {
+//                 left = charIndexMap.get(currentChar) + 1;
+//             }
+//             charIndexMap.put(currentChar, right);
+//             maxLength = Math.max(maxLength, right - left + 1);
+//         }
+
+//         return maxLength;
+//     }
+// }
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int maxLength = 0;
-        Map<Character, Integer> charMap = new HashMap<>();
-        int left = 0;
+        int n = s.length(), l = 0;
         
-        for (int right = 0; right < n; right++) {
-            if (!charMap.containsKey(s.charAt(right)) || charMap.get(s.charAt(right)) < left) {
-                charMap.put(s.charAt(right), right);
-                maxLength = Math.max(maxLength, right - left + 1);
-            } else {
-                left = charMap.get(s.charAt(right)) + 1;
-                charMap.put(s.charAt(right), right);
+        int[] index = new int[128]; // Assuming ASCII characters
+
+        for (int i = 0, j = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (index[ch] != 0) {
+                j = Math.max(j, index[ch]);
             }
+            index[ch] = i + 1; // Store the index + 1 to differentiate from default 0
+            l = Math.max(l, i - j + 1);
         }
-        
-        return maxLength;
+        return l;
     }
 }
