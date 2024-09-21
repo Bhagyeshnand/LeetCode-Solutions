8006593aa0d4a788792df05b906e9a5ea80bd255
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-09-21 23:33:35 +0530

    Time: 5 ms (100%), Space: 48.4 MB (11.68%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
@@ -1,27 +1,26 @@
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
-         Arrays.sort(nums);
-
-        int low = 1, high = nums[nums.length - 1],myAns=1;
-
-        while (low <= high) {
-
-            int mid = (low + high) / 2;
-
-            int energy = 0;
-            for (int i = 0; i < nums.length; i++) {
-
-                energy = energy + (int)Math.ceil((double)nums[i] / (double)mid);
-
+              int left = 1, right = 1000000;
+        
+        while(left <= right){
+            int mid = left+(right-left)/2;
+            long sum = getDivSum(nums, mid);
+            if(sum > threshold){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-            if (energy <= threshold) {
-                myAns = mid;
-                high = mid - 1;
-            } else
-                low = mid + 1;
-
         }
         
-        return myAns;
+        return left;
     }
-}
+    
+    private long getDivSum(int[] nums, int divisor){
+        long sum = 0;
+        for(int num: nums){
+            sum += (num-1)/divisor+1; 
+        }
+        
+        return sum;
+        }
+    }
