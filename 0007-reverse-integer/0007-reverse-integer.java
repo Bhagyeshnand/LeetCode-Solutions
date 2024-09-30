2cd225e3ed961a83bd3f42c73a06530e70d36c1e
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-09-30 20:07:46 +0530

    Time: 1 ms (84.73%), Space: 40.8 MB (46.2%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,16 +1,30 @@
 class Solution {
     public int reverse(int x) {
-        int res =0;
-        boolean isN = x < 0;
-        String s = String.valueOf(Math.abs(x));
-        StringBuilder sb = new StringBuilder(s).reverse();
-
-        try{
-            res = Integer.parseInt(sb.toString());
-        }catch(NumberFormatException e){
-            return 0;
+        int reversed = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) return 0;
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) return 0;
+            reversed = reversed * 10 + digit;
         }
-
-    return isN ? -res : res; 
+        return reversed;       
     }
 }
+
+// class Solution {
+//     public int reverse(int x) {
+//         int res =0;
+//         boolean isN = x < 0;
+//         String s = String.valueOf(Math.abs(x));
+//         StringBuilder sb = new StringBuilder(s).reverse();
+
+//         try{
+//             res = Integer.parseInt(sb.toString());
+//         }catch(NumberFormatException e){
+//             return 0;
+//         }
+
+//     return isN ? -res : res; 
+//     }
+// }
