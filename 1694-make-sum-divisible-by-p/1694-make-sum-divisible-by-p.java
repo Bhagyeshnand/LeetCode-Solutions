10e2613d7c6791b766637eb42474a79ee4b20d1e
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-10-03 23:08:23 +0530

    Time: 19 ms (99.53%), Space: 55.8 MB (96.24%) - LeetHub

--- 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.java
+++ 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.java
@@ -1,25 +1,27 @@
 class Solution {
     public int minSubarray(int[] nums, int p) {
-    int sum = 0;    
-    for(int x : nums)
-    sum = (sum + x)%p;
-  
-    int mod = sum%p; 
-    if(mod==0)
-    return 0;
-
-    int ans = nums.length;
-    HashMap<Integer, Integer>hm = new HashMap<>();
-    hm.put(0, -1);
-    int currSumMod = 0;
-    for(int i=0; i<nums.length; i++)
-    {
-      currSumMod = (currSumMod+nums[i])%p;
-      int neededMod = (currSumMod - mod + p)%p;
-      if(hm.containsKey(neededMod))
-      ans = Math.min(ans, i-hm.get(neededMod));
-      hm.put(currSumMod, i);
-    }
-     return ans==nums.length ? -1 : ans;
+        int n = nums.length;
+        double sum = 0;
+        for (int i : nums)
+            sum += i;
+        if (sum % p == 0.0)
+            return 0;
+        HashMap<Integer, Integer> mh = new HashMap<>();
+        sum %= p;
+        int s = (int)(sum);
+        int len = n, mod = 0;
+        for (int i = 0; i < n; i++) {
+            if (len == 1)
+                return 1;
+            mod = (nums[i] + mod) % p;
+            mh.put(mod, i);
+            if (s == mod)
+                len = Math.min(i + 1, len);
+            if (mh.get((mod + p - s) % p) != null) 
+                len = Math.min(len, i -  mh.get((mod + p - s) % p));
+        }
+        if(len==n)
+            return -1;
+        return len;
     }
 }
