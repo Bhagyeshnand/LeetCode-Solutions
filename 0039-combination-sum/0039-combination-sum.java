7ba6b1fac70220eebc50dc6af4a42ecbd65d50d0
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-10-07 22:39:25 +0530

    Time: 0 ms (100%), Space: 44.6 MB (35.87%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,20 +1,45 @@
+import java.util.AbstractList;
 class Solution {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, target, 0);
-    return list;
-}
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        return new AbstractList<List<Integer>>() {
+            List<List<Integer>> ret;
+            List<Integer> list;
+            @Override
+            public List<Integer> get(int index) {
+                if (ret == null) {
+                    init();
+                }
+                return ret.get(index);
+            }
 
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-    if(remain < 0) return;
-    else if(remain == 0) list.add(new ArrayList<>(tempList));
-    else{ 
-        for(int i = start; i < nums.length; i++){
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
-            tempList.remove(tempList.size() - 1);
-        }
+            void init() {
+                ret = new ArrayList<>();
+                list = new ArrayList<>();
+                Arrays.sort(candidates);
+                rec(0, candidates, target);
+            }
+            void rec(int start, int[] candidates, int target) {
+                if (target == 0) {
+                    ret.add(new ArrayList<>(list));
+                    return;
+                }
+
+                for (int i = start; i < candidates.length; i++) {
+                    if (candidates[i] > target) {
+                        break;
+                    }
+                    list.add(candidates[i]);
+                    rec(i, candidates, target - candidates[i]);
+                    list.remove(list.size() - 1);
+                }
+            }
+            @Override
+            public int size() {
+                if (ret == null) {
+                    init();
+                }
+                return ret.size();
+            }
+        };
     }
-}
 }
