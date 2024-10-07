372858a35e41cb1e3d3d046234def383473feb87
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-10-07 22:35:50 +0530

    Time: 1 ms (100%), Space: 43.1 MB (71.14%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -1,26 +1,44 @@
 class Solution {
     public int minLength(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char cur_char = s.charAt(i);
-
-            if (stack.isEmpty()) {
-                stack.push(cur_char);
-                continue;
-            }
-      
-            if (cur_char == 'B' && stack.peek() == 'A') {
-                stack.pop();
-            }
-            else if (cur_char == 'D' && stack.peek() == 'C') {
-                stack.pop();
-            }
-            else {
-                stack.push(cur_char);
+        char[] stack = new char[s.length()+1];
+        int last = -1;
+        for (char c : s.toCharArray()) {
+            if (last > -1 && (c == 'B' && stack[last] == 'A' || 
+                                     c == 'D' && stack[last] == 'C')) 
+            {
+                last--;
+            } else {
+                last++;
+                stack[last] = c;
             }
         }
-
-        return stack.size();
+        return last+1;
     }
 }
+
+// class Solution {
+//     public int minLength(String s) {
+//         Stack<Character> stack = new Stack<>();
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char cur_char = s.charAt(i);
+
+//             if (stack.isEmpty()) {
+//                 stack.push(cur_char);
+//                 continue;
+//             }
+      
+//             if (cur_char == 'B' && stack.peek() == 'A') {
+//                 stack.pop();
+//             }
+//             else if (cur_char == 'D' && stack.peek() == 'C') {
+//                 stack.pop();
+//             }
+//             else {
+//                 stack.push(cur_char);
+//             }
+//         }
+
+//         return stack.size();
+//     }
+// }
