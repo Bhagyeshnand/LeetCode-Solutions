5a2d878745c840f6d1857ab65e899cc5b4072cfe
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-10-10 21:20:51 +0530

    Time: 3 ms (97.76%), Space: 53.7 MB (65.92%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.java
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.java
@@ -1,24 +1,25 @@
 class Solution {
     public int maxWidthRamp(int[] nums) {
-        int n = nums.length;
-        Stack<Integer> stack = new Stack<>();
-        
-        // Step 1: Build a decreasing stack of indices
-        for (int i = 0; i < n; ++i) {
-            if (stack.isEmpty() || nums[stack.peek()] > nums[i]) {
-                stack.push(i);
-            }
+        int low = 1, high = nums.length-1, res = 0;
+
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(possible(nums, mid)) {
+                res = mid;
+                low = mid + 1;
+            } else high = mid - 1;
         }
-        
-        int maxWidth = 0;
-        
-        // Step 2: Traverse from the end and find maximum width ramp
-        for (int j = n - 1; j >= 0; --j) {
-            while (!stack.isEmpty() && nums[stack.peek()] <= nums[j]) {
-                maxWidth = Math.max(maxWidth, j - stack.pop());
-            }
-        }
-        
-        return maxWidth;
+        return res;
+    }
+
+    private boolean possible(int[] nums, int width) {
+        int i=0,j=width;
+        int min = nums[i];
+        while(j < nums.length) {
+            if(nums[j] >= min) return true;
+            j++;
+            min = Math.min(min, nums[++i]);
+        }   
+        return false;
     }
 }
