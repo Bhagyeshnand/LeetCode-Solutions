4f94793c127262c6b374374fc850de88b1d954fb
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-10-12 09:44:01 +0530

    Time: 25 ms (97.55%), Space: 94.9 MB (13.59%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.java
@@ -1,30 +1,32 @@
 class Solution {
+
     public int minGroups(int[][] intervals) {
-        int n = intervals.length;
-        int[] start_times = new int[n];
-        int[] end_times = new int[n];
-
-        // Extract start and end times
-        for (int i = 0; i < n; i++) {
-            start_times[i] = intervals[i][0];
-            end_times[i] = intervals[i][1];
+        // Find the minimum and maximum value in the intervals
+        int rangeStart = Integer.MAX_VALUE;
+        int rangeEnd = Integer.MIN_VALUE;
+        for (int[] interval : intervals) {
+            rangeStart = Math.min(rangeStart, interval[0]);
+            rangeEnd = Math.max(rangeEnd, interval[1]);
         }
 
-        // Sort start and end times
-        Arrays.sort(start_times);
-        Arrays.sort(end_times);
-
-        int end_ptr = 0, group_count = 0;
-
-        // Traverse through the start times
-        for (int start : start_times) {
-            if (start > end_times[end_ptr]) {
-                end_ptr++;
-            } else {
-                group_count++;
-            }
+        // Initialize the array with all zeroes
+        int[] pointToCount = new int[rangeEnd + 2];
+        for (int[] interval : intervals) {
+            pointToCount[interval[0]]++; // Increment at the start of the interval
+            pointToCount[interval[1] + 1]--; // Decrement right after the end of the interval
         }
 
-        return group_count;
+        int concurrentIntervals = 0;
+        int maxConcurrentIntervals = 0;
+        for (int i = rangeStart; i <= rangeEnd; i++) {
+            // Update currently active intervals
+            concurrentIntervals += pointToCount[i];
+            maxConcurrentIntervals = Math.max(
+                maxConcurrentIntervals,
+                concurrentIntervals
+            );
+        }
+
+        return maxConcurrentIntervals;
     }
 }
