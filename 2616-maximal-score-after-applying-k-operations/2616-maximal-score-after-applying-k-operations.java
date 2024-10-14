328a24e98d6e3df8f47fafd0235ac88f053a3dd7
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-10-14 20:04:36 +0530

    Time: 115 ms (98.1%), Space: 63.3 MB (27.01%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.java
@@ -1,19 +1,15 @@
 class Solution {
     public long maxKelements(int[] nums, int k) {
-        PriorityQueue <Integer> pq = new PriorityQueue<>((a,b)->b-a);
-
-        for(int n : nums) pq.offer(n);
-
-        long score = 0;
-
-        while( k > 0){
-            int big = pq.poll();
-            score += big;
-            int update = (int)Math.ceil(big/3.0);
-
-            pq.add(update);
-            k--;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for(int i : nums){
+            pq.add(i);
         }
-        return score;
+        long ans = 0;
+        while(k-- > 0){
+            int n = pq.poll();
+            ans += n;
+            pq.offer((n + 2)/ 3);
+        }
+        return ans;
     }
 }
