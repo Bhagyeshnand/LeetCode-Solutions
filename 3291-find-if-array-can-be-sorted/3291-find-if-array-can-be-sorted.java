5fef4f1728cf2ca9343e9ac2fa71f29f1dcd8332
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-11-06 10:31:54 +0530

    Time: 1 ms (100%), Space: 44.4 MB (35.22%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
@@ -1,20 +1,51 @@
-class Solution {
-    public static boolean canSortArray(final int[] nums) {
-        short pmax = 0, cmin = 0, cmax = 0;
-        byte pcnt = 0;
-        for (final int v : nums) {
-            final byte ccnt = (byte)Integer.bitCount(v);
-            if (pcnt == ccnt) {
-                cmin = cmin > v ? (short)v : cmin;
-                cmax = cmax < v ? (short)v : cmax;
-            } else if (cmin < pmax) {
-                return false;
-            } else {
-                pmax = cmax;
-                cmin = cmax = (short)v;
-                pcnt = ccnt;
+class Solution 
+{
+    public boolean canSortArray(int[] nums) 
+    {
+        int n = nums.length;
+
+        // Copy the original array to values
+        int[] values = Arrays.copyOf(nums, n);
+
+        // First Pass: Iterate from left to right
+        // Goal: Move the maximum value of each segment as far right as possible
+        for (int i = 0; i < n - 1; i++) 
+        {
+            if (values[i] <= values[i + 1]) continue;
+            else 
+            {
+                // Count the number of set bits using Integer.bitCount
+                if (Integer.bitCount(values[i]) == Integer.bitCount(values[i + 1])) 
+                {
+                    // Swap them if they have the same number of set bits
+                    int temp = values[i];
+                    values[i] = values[i + 1];
+                    values[i + 1] = temp;
+                } 
+                else return false; // Return false if they cannot be swapped
             }
         }
-        return cmin >= pmax;
+
+        // Second Pass: Iterate from right to left
+        // Goal: Move the minimum value of each segment as far left as possible
+        for (int i = n - 1; i >= 1; i--) 
+        {
+            if (values[i] >= values[i - 1]) continue;
+            else 
+            {
+                // Count the number of set bits using Integer.bitCount
+                if (Integer.bitCount(values[i]) == Integer.bitCount(values[i - 1])) 
+                {
+                    // Swap them if they have the same number of set bits
+                    int temp = values[i];
+                    values[i] = values[i - 1];
+                    values[i - 1] = temp;
+                } 
+                else return false; // Return false if they cannot be swapped
+            }
+        }
+
+        // If both passes complete without returning false, the array can be sorted
+        return true;
     }
 }
