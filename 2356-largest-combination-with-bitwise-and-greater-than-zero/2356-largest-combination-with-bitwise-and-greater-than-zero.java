97c4cc573810e9697254239979af6752dc98329c
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-11-07 19:42:39 +0530

    Time: 9 ms (100%), Space: 56 MB (93.23%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.java
@@ -1,13 +1,23 @@
 class Solution {
     public int largestCombination(int[] candidates) {
-        int ans = 0;
-        for (int i = 0; i < 32; i++) {
-            int cnt = 0;
-            for (int candidate : candidates) {
-                if ((candidate & (1 << i)) != 0) cnt++;
-            }
-            ans = Math.max(ans, cnt);
+       int[] ans=new int[32];
+       for(int x:candidates){
+           find(x,ans);
+       } 
+        int res=0;
+       for(int i=0;i<32;i++){
+           res=Math.max(res,ans[i]);//traverse the bit array and check the max value
+       }
+       return res;
+    }
+    public void find(int n,int[] ans){
+
+        int j=31;//index for updating the values in bit array
+        while(n>0){
+            int a=(n&1);//for checking if bit is set or not
+            ans[j]+=a;
+            n>>=1;//left shift the given no
+            j--;
         }
-        return ans;
     }
 }
