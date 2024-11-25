919442d8f26a1b535dcabbad2b8ff69941faad1f
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2024-11-25 08:02:31 +0530

    Time: 3 ms (100%), Space: 43.1 MB (77.24%) - LeetHub

--- 0787-sliding-puzzle/0787-sliding-puzzle.java
+++ 0787-sliding-puzzle/0787-sliding-puzzle.java
@@ -1,106 +1,49 @@
-/**
- * Double-Ended BFS + Bit Manipulation
- *
- * Find possible zero moves for each zero location. Use this to find next states
- * of current state. Perform Double-Ended BFS to find the total number of moves.
- *
- * To save space, we are using a single integer to represent a state. Since our
- * number is between 0 & 5, it will take maximum 3 bits for each number. Thus we
- * will need only 18 bits to store each state.
- *
- * Time Complexity: O(V + E).
- * V = Number of possible states = (M*N)!.
- * E = 3 * V, because there can be a maximum of 3 edges from each vertex.
- * Total TC = O((MN)! + 3*(MN)!) = O((MN)!) = O(4 * 6!)
- *
- * Space Complexity: O((MN)!) to save each state in begin, end and visited
- * collections = O(6!)
- *
- * M = Number of rows. N = Number of cols.
- */
 class Solution {
-    // 3-Bit Binary representation of 123450
-    private static final int FINAL_STATE = 0b001010011100101000;
-    // Possible destination of zero. Here DIRS[0] represents the most significant
-    // 3-Bit number in the state. And, DIRS[5] represents the least significant
-    // 3-Bit number in the state.
-    private static final int[][] DIRS = { { 1, 3 }, { 0, 2, 4 }, { 1, 5 }, { 0, 4 }, { 1, 3, 5 }, { 2, 4 } };
+    int min = Integer.MAX_VALUE;
+    int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     public int slidingPuzzle(int[][] board) {
-        if (board == null || board.length != 2 || board[0].length != 3) {
-            throw new IllegalArgumentException("Input board is invalid");
-        }
-
-        int zeroIdx = -1;
-        int curState = 0;
+        int[] zero = {-1, -1};
+        outer:
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 3; j++) {
-                // Inserting the num at the end of integer
-                curState = (curState << 3) | board[i][j];
                 if (board[i][j] == 0) {
-                    zeroIdx = i * 3 + j;
+                    zero = new int[]{i, j};
+                    break outer;
                 }
             }
         }
-
-        if (FINAL_STATE == curState) {
-            return 0;
-        }
-
-        HashSet<Integer> visited = new HashSet<>();
-        int moves = 0;
-
-        HashMap<Integer, Integer> begin = new HashMap<>();
-        begin.put(curState, zeroIdx);
-        visited.add(curState);
-
-        HashMap<Integer, Integer> end = new HashMap<>();
-        end.put(FINAL_STATE, 5);
-        visited.add(FINAL_STATE);
-
-        while (!begin.isEmpty()) {
-            if (begin.size() > end.size()) {
-                HashMap<Integer, Integer> tempSet = begin;
-                begin = end;
-                end = tempSet;
-            }
-
-            HashMap<Integer, Integer> next = new HashMap<>();
-            moves++;
-            for (int cur : begin.keySet()) {
-                zeroIdx = begin.get(cur);
-
-                for (int d : DIRS[zeroIdx]) {
-                    int newState = swap(cur, zeroIdx, d);
-                    if (end.containsKey(newState)) {
-                        return moves;
-                    }
-                    if (visited.add(newState)) {
-                        next.put(newState, d);
-                    }
-                }
-            }
-            begin = next;
-        }
-
-        return -1;
+        helper_backtrack(board, 0, new int[]{-1, -1}, zero);
+        return min == Integer.MAX_VALUE ? -1 : min;
     }
-
-    private int swap(int state, int zeroIdx, int destIdx) {
-        // Generate mask for destIdx
-        int mask = 0b111 << ((5 - destIdx) * 3);
-        // Extracting num at destIdx
-        int num = state & mask;
-        // Moving num to zeroIdx
-        if (zeroIdx < destIdx) {
-            num <<= (destIdx - zeroIdx) * 3;
-        } else {
-            num >>>= (zeroIdx - destIdx) * 3;
+    public void helper_backtrack(int[][] board, int moves, int[] last, int[] curr) {
+        if (moves >= 20) return;
+        if (helper_isDone(board)) {
+            min = Math.min(min, moves);
+            return;
         }
-
-        // Setting zero at destIdx
-        state &= ~mask;
-        // Setting num at zeroIdx
-        return state | num;
+        for (int[] dir : dirs) {
+            int i = curr[0] + dir[0];
+            int j = curr[1] + dir[1];
+            if (i < 0 || i >= 2 || j < 0 || j >= 3 || (last[0] == i && last[1] == j)) continue;
+            int[] newMove = {i, j};
+            helper_flip(board, curr, newMove);
+            helper_backtrack(board, moves + 1, curr, newMove);
+            helper_flip(board, curr, newMove);
+        }
+    }
+    public void helper_flip(int[][] board, int[] f, int[] s) {
+        int temp = board[f[0]][f[1]];
+        board[f[0]][f[1]] = board[s[0]][s[1]];
+        board[s[0]][s[1]] = temp;
+    }
+    public boolean helper_isDone(int[][] board) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 2) return true;
+                if (board[i][j] != 3 * i + j + 1) return false;
+            }
+        }
+        return true;
     }
 }
