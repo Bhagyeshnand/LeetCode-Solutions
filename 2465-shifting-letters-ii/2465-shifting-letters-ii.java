71b77348fcbc7225904c60393d71f22a406b67e8
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-01-05 13:54:00 +0530

    Time: 5 ms (100%), Space: 75.9 MB (25.94%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.java
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.java
@@ -1,26 +1,23 @@
 class Solution {
     public String shiftingLetters(String s, int[][] shifts) {
-        int n = s.length();
-        int[] shift = new int[n + 1];
-
-        for (int[] shiftOp : shifts) {
-            int start = shiftOp[0], end = shiftOp[1], direction = shiftOp[2];
-            shift[start] += (direction == 1 ? 1 : -1);
-            if (end + 1 < n) shift[end + 1] -= (direction == 1 ? 1 : -1);
+        char arr[]=s.toCharArray();
+        int[] map=new int[arr.length+1];
+        for(int[] num:shifts){
+            if(num[2]==0){
+                map[num[0]]-=1;
+                map[num[1]+1]+=1;
+            }else{
+                map[num[0]]+=1;
+                map[num[1]+1]-=1;
+            }
         }
-
-        int currentShift = 0;
-        for (int i = 0; i < n; ++i) {
-            currentShift += shift[i];
-            shift[i] = currentShift;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=map[i];
+            int n=((arr[i]-'a')+sum)%26;
+            if(n<0) n+=26;
+            arr[i]=(char)(n+'a');
         }
-
-        StringBuilder result = new StringBuilder(s);
-        for (int i = 0; i < n; ++i) {
-            int netShift = (shift[i] % 26 + 26) % 26;
-            result.setCharAt(i, (char) ('a' + (s.charAt(i) - 'a' + netShift) % 26));
-        }
-
-        return result.toString();
+        return new String(arr);
     }
 }
