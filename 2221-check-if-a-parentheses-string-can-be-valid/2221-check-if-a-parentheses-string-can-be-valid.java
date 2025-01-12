53994c4cfeecfdc311c15028d05e6dadaf79f2a0
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-01-12 20:45:17 +0530

    Time: 9 ms (100%), Space: 46.2 MB (15.77%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
@@ -1,44 +1,31 @@
-import java.util.Stack;
-
 class Solution {
     public boolean canBeValid(String s, String locked) {
-        int stringLength = s.length();
-        if (stringLength % 2 == 1) {
+        int n = s.length();
+        if (n % 2 != 0) {
             return false;
         }
-
-        Stack<Integer> openIndices = new Stack<>();
-        Stack<Integer> unlockedIndices = new Stack<>();
-
-        // Traverse the string
-        for (int i = 0; i < stringLength; i++) {
-            if (locked.charAt(i) == '0') {
-                unlockedIndices.push(i);
-            } else if (s.charAt(i) == '(') {
-                openIndices.push(i);
-            } else if (s.charAt(i) == ')') {
-                if (!openIndices.isEmpty()) {
-                    openIndices.pop();
-                } else if (!unlockedIndices.isEmpty()) {
-                    unlockedIndices.pop();
+        int upper = 0;
+        int lower = 0;
+        for (int i = 0; i < n; i++) {
+            if (locked.charAt(i) == '1') {
+                if (s.charAt(i) == '(') {
+                    lower++;
+                    upper++;
                 } else {
-                    return false;
+                    lower--;
+                    upper--;
                 }
+            } else {
+                upper++;
+                lower--;
+            }
+            if (lower < 0) {
+                lower += 2;
+            }
+            if (upper < 0) {
+                return false;
             }
         }
-
-        // Match remaining open parentheses
-        while (!openIndices.isEmpty() && !unlockedIndices.isEmpty() &&
-               openIndices.peek() < unlockedIndices.peek()) {
-            openIndices.pop();
-            unlockedIndices.pop();
-        }
-
-        // Final check
-        if (openIndices.isEmpty() && !unlockedIndices.isEmpty()) {
-            return unlockedIndices.size() % 2 == 0;
-        }
-
-        return openIndices.isEmpty();
+        return lower == 0;
     }
 }
