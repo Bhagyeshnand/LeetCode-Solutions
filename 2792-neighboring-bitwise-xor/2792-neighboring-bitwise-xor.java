bb03b7c2aa1c34fb462d31ec0b60eb0f32bae7fb
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-01-17 17:15:48 +0530

    Time: 2 ms (100%), Space: 62.6 MB (26.47%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -1,19 +1,9 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
-        int n=derived.length;
-        int[] org=new int[n];
-
-        //Populating the org array
-        for(int i=1; i<n; i++){
-            org[i]=derived[i-1]^org[i-1];
+        int res = 0;
+        for (int x : derived) {
+            res ^= x;
         }
-
-        //Validating the rules for index i
-        for(int i=0; i<n; i++){
-            int idx=(i+1)%n;
-            if(derived[i]!=(org[i]^org[idx])) return false;
-        }
-
-        return true;
+        return res == 0;
     }
 }
