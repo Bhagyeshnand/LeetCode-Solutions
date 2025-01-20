d945b31cc988d462afea50393fad2457950eaefe
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-01-20 07:44:30 +0530

    Time: 2 ms (100%), Space: 58.5 MB (99.5%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
@@ -1,22 +1,24 @@
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
-        int rows = mat.length, cols = mat[0].length;
-        Map<Integer, int[]> positionMap = new HashMap<>();
-        int[] rowCount = new int[rows];
-        int[] colCount = new int[cols];
-        Arrays.fill(rowCount, cols);
-        Arrays.fill(colCount, rows);
-        for (int r = 0; r < rows; ++r) {
-            for (int c = 0; c < cols; ++c) {
-                positionMap.put(mat[r][c], new int[]{r, c});
-            }
+        int[] map = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            map[arr[i]] = i;
         }
-        for (int idx = 0; idx < arr.length; ++idx) {
-            int[] pos = positionMap.get(arr[idx]);
-            if (--rowCount[pos[0]] == 0 || --colCount[pos[1]] == 0) {
-                return idx;
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < mat.length; i++) {
+            int max = 0;
+            for (int j = 0; j < mat[i].length; j++) {
+                max = Math.max(max, map[mat[i][j]]);
             }
+            ans = Math.min(ans, max);
         }
-        return -1;
+        for (int i = 0; i < mat[0].length; i++) {
+            int max = 0;
+            for (int j = 0; j < mat.length; j++) {
+                max = Math.max(max, map[mat[j][i]]);
+            }
+            ans = Math.min(ans, max);
+        }
+        return ans;
     }
 }
