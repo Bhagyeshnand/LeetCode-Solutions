6ff977eb5b3feb79c8f1381a243841433187e815
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-02-01 06:45:51 +0530

    Time: 2 ms (100%), Space: 55.9 MB (96.68%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,37 +1,20 @@
-public class Solution {
+class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int left = 0, right = nums.length - 1;
-        Random rand = new Random();
-        while (true) {
-            int pivot_index = left + rand.nextInt(right - left + 1);
-            int new_pivot_index = partition(nums, left, right, pivot_index);
-            if (new_pivot_index == nums.length - k) {
-                return nums[new_pivot_index];
-            } else if (new_pivot_index > nums.length - k) {
-                right = new_pivot_index - 1;
-            } else {
-                left = new_pivot_index + 1;
+        int[] map = new int[20001];
+        for (int num : nums) {
+            map[num + 10000]++;
+        }
+        int result = 0;
+        for (int i = map.length - 1; i > 0; i--) {
+            if (map[i] != 0) {
+                if (k <= map[i]) {
+                    result = i - 10000;
+                    break;
+                } else {
+                    k -= map[i];
+                }
             }
         }
-    }
-
-    private int partition(int[] nums, int left, int right, int pivot_index) {
-        int pivot = nums[pivot_index];
-        swap(nums, pivot_index, right);
-        int stored_index = left;
-        for (int i = left; i < right; i++) {
-            if (nums[i] < pivot) {
-                swap(nums, i, stored_index);
-                stored_index++;
-            }
-        }
-        swap(nums, right, stored_index);
-        return stored_index;
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        return result;
     }
 }
