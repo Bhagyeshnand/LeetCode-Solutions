7a46a70d43cd32f31300feff9e0c663a3884a144
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-02-06 12:49:49 +0530

    Time: 2 ms (100%), Space: 56.8 MB (54.03%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,59 +1,41 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-public class Solution {
-  
-  public ListNode sortList(ListNode head) {
-    if (head == null || head.next == null)
-      return head;
-        
-    // step 1. cut the list to two halves
-    ListNode prev = null, slow = head, fast = head;
-    
-    while (fast != null && fast.next != null) {
-      prev = slow;
-      slow = slow.next;
-      fast = fast.next.next;
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null) return null;
+        return quickSort(head);
     }
-    
-    prev.next = null;
-    
-    // step 2. sort each half
-    ListNode l1 = sortList(head);
-    ListNode l2 = sortList(slow);
-    
-    // step 3. merge l1 and l2
-    return merge(l1, l2);
-  }
-  
-  ListNode merge(ListNode l1, ListNode l2) {
-    ListNode l = new ListNode(0), p = l;
-    
-    while (l1 != null && l2 != null) {
-      if (l1.val < l2.val) {
-        p.next = l1;
-        l1 = l1.next;
-      } else {
-        p.next = l2;
-        l2 = l2.next;
-      }
-      p = p.next;
-    }
-    
-    if (l1 != null)
-      p.next = l1;
-    
-    if (l2 != null)
-      p.next = l2;
-    
-    return l.next;
-  }
 
+    private ListNode quickSort(ListNode head) {
+        return quickSort(head, null);
+    }
+
+    private ListNode quickSort(ListNode head, ListNode stop) {
+        if (head == null || head.next == null || head == stop || head.next == stop) {
+            return head;
+        }
+        ListNode lowHead = head;
+        ListNode prev = head;
+        ListNode tail = prev.next;
+        boolean isSorted = true;
+        while (tail != stop) {
+            isSorted = isSorted && prev.val <= tail.val;
+            if (tail.val < head.val) {
+                prev.next = tail.next;
+                tail.next = lowHead;
+                lowHead = tail;
+            } else {
+                prev = prev.next;
+            }
+            tail = prev.next;
+        }
+
+        if (isSorted) {
+            return head;
+        }
+
+        ListNode result = quickSort(lowHead, head);
+
+        head.next = quickSort(head.next, stop);
+
+        return result;
+    }
 }
