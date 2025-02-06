8e2c22be4b97cbc1463aff62ca13fa6b340558de
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-02-06 12:39:06 +0530

    Time: 162 ms (96.15%), Space: 68.6 MB (69.23%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.java
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.java
@@ -1,17 +1,14 @@
-import java.util.HashMap;
-
 class Solution {
     public int tupleSameProduct(int[] nums) {
-        HashMap<Integer, Integer> productCount = new HashMap<>();
-        int result = 0;
-
-        for (int i = 0; i < nums.length; i++)
-            for (int j = i+1; j < nums.length; j++) {
-                int product = nums[i] * nums[j];
-                result += 8 * productCount.getOrDefault(product, 0);
-                productCount.put(product, productCount.getOrDefault(product, 0) + 1);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int product = nums[i]*nums[j];
+                count+=(map.getOrDefault(product,0)*8);
+                map.merge(product,1,Integer::sum);
             }
-
-        return result;
+        }
+        return count;
     }
 }
