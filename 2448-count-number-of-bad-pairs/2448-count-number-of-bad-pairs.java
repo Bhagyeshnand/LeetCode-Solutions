d380dcf378235b5026a194ebc46bf3b619350387
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-02-09 12:13:42 +0530

    Time: 27 ms (100%), Space: 59.4 MB (32.06%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -1,33 +1,11 @@
 class Solution {
     public long countBadPairs(int[] nums) {
-         long goodPairs = 0;
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            int diff = nums[i] - i;
-            if (map.containsKey(diff)) {
-                goodPairs += map.get(diff);
-            }
-            map.put(diff, map.getOrDefault(diff, 0) + 1);
+        Map<Integer, Integer> bag = new HashMap<>();
+        long count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count += -1 + bag.merge(nums[i] - i, 1, Integer::sum);
         }
-
-        long totalPairs = (long) n * (n - 1) / 2;
-        return totalPairs - goodPairs;
+        int len = nums.length;
+        return 1L * len * (len - 1) / 2 - count;
     }
 }
-
-/*
-Count Good pairs instead of Bad pairs
-Ans = Total pairs - No of Good pairs
-Number of total pairs where i<j = (n*(n-1))/2
-
-A pair is said to be good if i < j and j - i == nums[j] - nums[i]
-This can be rewritten as j - nums[j] == i- nums[i]
-
-Keep a track of this difference using a map
-
-If difference is found in future -
-It is a good pair, add its count to goodpair_count
-increment its count also in the map as well.
-*/
