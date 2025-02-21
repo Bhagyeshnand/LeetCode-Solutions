648e9d1c2e1c57b26072a6ee789d5d5529cf3ef1
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-02-21 19:02:34 +0530

    Time: 20 ms (98.92%), Space: 47.5 MB (52.83%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
@@ -14,70 +14,29 @@
  * }
  */
 class FindElements {
-    TreeNode groot;
+    BitSet recoveredValues;
 
     public FindElements(TreeNode root) {
-        if(root!=null && root.val==-1){
-            root.val=0;                
-        }
-        solve1(root);   
-        groot=root;  
-
-        //printTree(groot); //Used for printing the new Tree
+        root.val = 0;
+        recoveredValues = new BitSet();
+        recoverTree(root);
     }
-
-    void printTree(TreeNode node){
-        if(node==null){ 
-            System.out.print(" null ");
-            return;
-        }
-
-        System.out.print(" "+node.val);
-        printTree(node.left);
-        printTree(node.right);
-    }
-
-
-
-    void solve1(TreeNode node){
-        if(node==null){
-            return;
-        }
-
-        if(node.left !=null){
-            node.left.val=2*node.val+1;                
-        }
-        if(node.right !=null){
-            node.right.val=2*node.val+2;
-        }
-
-        solve1(node.left);
-        solve1(node.right);
-
-    }
-
-
     
     public boolean find(int target) {
-        TreeNode node=new TreeNode();
-        node=groot;
-        return solve2(node, target);
+        return recoveredValues.get(target);
     }
 
-    boolean solve2(TreeNode node, int target){
-        if(node==null){
-            return false;
+    private void recoverTree(TreeNode root) {
+        recoveredValues.set(root.val);
+        if(root.left != null) {
+            root.left.val = root.val * 2 + 1;
+            recoverTree(root.left);
         }
-        if(node.val==target){
-            return true;
+        if(root.right != null) {
+            root.right.val = root.val * 2 + 2;
+            recoverTree(root.right);
         }
-        
-        if(solve2(node.left, target) || solve2(node.right, target)) return true;
-
-        return false;
-    }  
-
-  
+    }
 }
 
 /**
