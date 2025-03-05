1219a7b2336b32904c36b53e71071878b3e23d62
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-03-05 13:09:10 +0530

    Time: 0 ms (100%), Space: 40.6 MB (37.3%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,9 +1,5 @@
 class Solution {
     public long coloredCells(int n) {
-        long sum=1; // Initial number of colored cells
-        for(int i=2; i<=n; i++){
-            sum+=(i-1)*4; // Increment by 4 times the previous level
-        }
-        return sum;
+        return 1+(long)2*n*(n-1);
     }
 }
