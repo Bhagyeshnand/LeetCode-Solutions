59129472a7e1acce1b46b26968935a533b1a9966
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-03-18 21:39:05 +0530

    Time: 3 ms (98.64%), Space: 55.4 MB (92.92%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.java
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -1,20 +1,35 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
-        int n = nums.length;
-        int maxLength = 1;
-        int left = 0;
-        int usedBits = 0;
+
+        int bit = 0;
+        int max = 1;
+
+         int s = 0;
+         int e = 0;
+         int n = nums.length;
+
+         while(e < n){
+
+
+          while( (bit& nums[e]) != 0){
+
+            bit^=nums[s++];
+
+          }
+
+
+          bit|=nums[e];
+
+          if(e-s+1 > max){
+            max = e-s+1;
+          }
+          e++;
+
+
+
+         }
+
+return max;
         
-        for (int right = 0; right < n; right++) {
-            while ((usedBits & nums[right]) != 0) {
-                usedBits ^= nums[left];
-                left++;
-            }
-            
-            usedBits |= nums[right];
-            maxLength = Math.max(maxLength, right - left + 1);
-        }
-        
-        return maxLength;
     }
 }
