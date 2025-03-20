2382dbd0ed71d759b0c1f999c5579f5dd893d927
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-03-20 05:49:08 +0530

    Time: 5 ms (100%), Space: 118.3 MB (82.94%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.java
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.java
@@ -1,45 +1,70 @@
-class DSU {
-    int[] parent, rank, weights;
-    public DSU(int n) {
-        parent = new int[n];
-        rank = new int[n];
-        weights = new int[n];
-        Arrays.fill(weights, 131071);
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
-    }
-    public int find(int x) {
-        if (x != parent[x])
-            parent[x] = find(parent[x]);
-        return parent[x];
-    }
-    public void union(int x, int y, int weight) {
-        int xx = find(x);
-        int yy = find(y);
-        if (rank[xx] < rank[yy])
-            parent[xx] = yy;
-        else
-            parent[yy] = xx;
-        weights[xx] = weights[yy] = weights[xx] & weights[yy] & weight;
-        if (rank[xx] == rank[yy])
-            rank[xx]++;
-    }
-    public int minimumCostOfWalk(int x, int y) {
-        if (x == y)
-            return 0;
-        if (find(x) != find(y))
-            return -1;
-        return weights[find(x)];
-    }
-}
 class Solution {
+
+    public class UnionFind{
+        public int[] p;
+        public int[] val; // & of all
+        public int[] d;
+
+        UnionFind(int n){
+            p = new int[n];
+            val = new int[n];
+            d = new int[n];
+            Arrays.fill(p, -1);
+            Arrays.fill(val, 0xFFFFFFF);
+            Arrays.fill(d, 0);
+        }
+
+        public  int findp(int u){
+            while(p[u] != -1) u = p[u];
+            return u;
+        }
+
+        public void union(int u, int v, int wt){
+            u = findp(u);
+            v = findp(v);
+
+            if(u == v){
+                val[u] &= wt;
+                return;
+            }
+
+            if(d[u] > d[v]){
+                p[v] = u;
+                val[u] &= wt;
+                val[u] &= val[v];   
+            }
+            else if(d[v] > d[u]){
+                p[u] = v;
+                val[v] &= wt;
+                val[v] &= val[u];
+            }
+            else{
+                p[v] = u;
+                val[u] &= wt;
+                val[u] &= val[v];
+                d[u]++;
+            }
+        }
+    }
+
     public int[] minimumCost(int n, int[][] edges, int[][] query) {
-        DSU uf = new DSU(n);
-        for (int[] edge : edges)
-            uf.union(edge[0], edge[1], edge[2]);
-        int[] result = new int[query.length];
-        for (int i = 0; i < query.length; i++)
-            result[i] = uf.minimumCostOfWalk(query[i][0], query[i][1]);
-        return result;
+        UnionFind uf = new UnionFind(n);
+
+        for(int[] e : edges)uf.union(e[0], e[1], e[2]);
+
+
+        int[] ans = new int[query.length];
+        int id = 0;
+
+        for(int[] q : query){
+            int p1 = uf.findp(q[0]);
+            int p2 = uf.findp(q[1]);
+            if(p1 == p2){
+                ans[id++] = uf.val[p1];
+            }
+            else ans[id++] = -1;
+        }
+
+        return ans;
     }
 }
