63e15070d8c944ca1fe57b06bd470388c3f0fc84
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-03-20 06:12:12 +0530

    Time: 0 ms (100%), Space: 46.5 MB (5.7%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -13,20 +13,46 @@
  *     }
  * }
  */
+import java.util.*;
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>(List.of(root));
-        List<Double> ans = new ArrayList<>();
-        while (q.size() > 0) {
-            double qlen = q.size(), row = 0;
-            for (int i = 0; i < qlen; i++) {
-                TreeNode curr = q.poll();
-                row += curr.val;
-                if (curr.left != null) q.offer(curr.left);
-                if (curr.right != null) q.offer(curr.right);
+        List<Double> res = new AbstractList<>(){
+            List<Double> temp = null;
+
+            @Override
+            public int size(){
+                if(temp==null){
+                    temp =  helper(root);
+                }
+                return temp.size();
             }
-            ans.add(row/qlen);
+
+            @Override
+            public Double get(int ind){
+                if(temp == null){
+                    temp = helper(root);
+                }
+                return temp.get(ind);
+            }
+        };
+        return res;
+    }
+
+    private List<Double> helper(TreeNode root){
+        List<Double> res = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int n = q.size();
+            double s=0.0;
+            for(int i=0;i<n;i++){
+                TreeNode curr = q.poll();
+                s += curr.val;
+                if(curr.left!=null) q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
+            }
+            res.add((s/n));
         }
-        return ans;
+        return res;
     }
 }
