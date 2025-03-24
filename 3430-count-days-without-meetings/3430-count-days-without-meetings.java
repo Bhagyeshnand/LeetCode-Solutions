020245b36f0455bebb94dcfa12b2fd5cafe09eeb
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-03-24 21:49:31 +0530

    Time: 14 ms (100%), Space: 112 MB (6.4%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.java
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -1,20 +1,40 @@
 class Solution {
-    public int countDays(int days, int[][] intervals) {
-  Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        LinkedList<int[]> merged = new LinkedList<>();
-        for (int[] interval : intervals) {
-            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
-                merged.add(interval);
-            } else {
-                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+    public int countDays(int days, int[][] meetings) {
+        int freeDays = days;
+        TreeMap<Integer, Integer> meetingDays = new TreeMap<>();
+        for (int[] meeting : meetings) {
+            int start = meeting[0];
+            int end = meeting[1];
+            // System.out.printf("Meeting: (%d, %d)\n", start, end);
+            int overlapDays = 0;
+
+            Map.Entry<Integer, Integer> previousMeetingDays = meetingDays.floorEntry(start);
+            if (previousMeetingDays != null && previousMeetingDays.getValue() >= start - 1) {
+                if (previousMeetingDays.getValue() >= end) continue; // Complete overlap
+                overlapDays = previousMeetingDays.getValue() - previousMeetingDays.getKey() + 1;
+                // System.out.printf("Overlapping previous: (%d, %d)\n", previousMeetingDays.getKey(), previousMeetingDays.getValue());
+                start = previousMeetingDays.getKey();
             }
-        }
 
-        int ans = 0;
-        for (int i = 0; i < merged.size(); i++) {
-            ans += merged.get(i)[1] - merged.get(i)[0] + 1;
-        }
+            Map.Entry<Integer, Integer> nextMeetingDays = meetingDays.ceilingEntry(start + 1);
+            while (nextMeetingDays != null && nextMeetingDays.getKey() <= end + 1) {
+                meetingDays.remove(nextMeetingDays.getKey());
+                overlapDays += nextMeetingDays.getValue() - nextMeetingDays.getKey() + 1;
+                // System.out.printf("Overlapping next: (%d, %d)\n", nextMeetingDays.getKey(), nextMeetingDays.getValue());
+                if (nextMeetingDays.getValue() >= end) {
+                    end = nextMeetingDays.getValue();
+                    break;
+                }
+                nextMeetingDays = meetingDays.ceilingEntry(start + 1);
+            }
 
-        return days - ans;
+            meetingDays.put(start, end);
+            freeDays -= (end - start + 1) - overlapDays;
+            // System.out.printf("Result: (%d, %d)\n", start, end);
+            // System.out.printf("FreeDays - ((%d - %d + 1) - %d) = %d\n\n", end, start, overlapDays, freeDays);
+            if (freeDays == 0) break;
+        }
+        return freeDays;
+
     }
 }
