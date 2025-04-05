55f0d25d50ff4059e4071609779f9cc7f68f6735
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-04-05 10:59:18 +0530

    Time: 0 ms (100%), Space: 44.1 MB (95.45%) - LeetHub

--- 0772-construct-quad-tree/0772-construct-quad-tree.java
+++ 0772-construct-quad-tree/0772-construct-quad-tree.java
@@ -1,25 +1,86 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
 class Solution {
-  public Node construct(int[][] grid) {
-    return helper(grid, 0, 0, grid.length);
-  }
+    public Node construct(int[][] grid) {
+        return buildQuadTree(grid, 0, 0, grid.length);
+    }
 
-  private Node helper(int[][] grid, int i, int j, int w) {
-    if (allSame(grid, i, j, w))
-      return new Node(grid[i][j] == 1 ? true : false, true);
+    private Node buildQuadTree(int[][] grid, int row, int col, int size) {
+        // Base Case: If all values in the sub-grid are the same, return a leaf node
+        if (isUniform(grid, row, col, size)) {
+            return new Node(grid[row][col] == 1, true);
+        }
 
-    Node node = new Node(true, false);
-    node.topLeft = helper(grid, i, j, w / 2);
-    node.topRight = helper(grid, i, j + w / 2, w / 2);
-    node.bottomLeft = helper(grid, i + w / 2, j, w / 2);
-    node.bottomRight = helper(grid, i + w / 2, j + w / 2, w / 2);
-    return node;
-  }
+        // Recursive Case: Divide into four quadrants
+        int half = size / 2;
+        Node topLeft = buildQuadTree(grid, row, col, half);
+        Node topRight = buildQuadTree(grid, row, col + half, half);
+        Node bottomLeft = buildQuadTree(grid, row + half, col, half);
+        Node bottomRight = buildQuadTree(grid, row + half, col + half, half);
 
-  private boolean allSame(int[][] grid, int i, int j, int w) {
-    for (int x = i; x < i + w; ++x)
-      for (int y = j; y < j + w; ++y)
-        if (grid[x][y] != grid[i][j])
-          return false;
-    return true;
-  }
+        return new Node(true, false, topLeft, topRight, bottomLeft, bottomRight);
+    }
+
+    private boolean isUniform(int[][] grid, int row, int col, int size) {
+        int value = grid[row][col];
+        for (int i = row; i < row + size; i++) {
+            for (int j = col; j < col + size; j++) {
+                if (grid[i][j] != value) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+     public void printQuadTree(Node root) {
+    if (root == null) return;
+    if (root.isLeaf) {
+        System.out.println("Leaf Node: " + (root.val ? "1" : "0"));
+    } else {
+        System.out.println("Parent Node");
+        printQuadTree(root.topLeft);
+        printQuadTree(root.topRight);
+        printQuadTree(root.bottomLeft);
+        printQuadTree(root.bottomRight);
+        }
+}
 }
