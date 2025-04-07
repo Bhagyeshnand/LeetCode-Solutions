7c24493f3954a2b532f4d72588adaf3c937b366a
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-04-07 17:03:12 +0530

    Time: 4 ms (100%), Space: 42.3 MB (82.54%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,31 +1,79 @@
 class Solution {
-    
+
+    // private boolean solve(int idx, int k, int[] nums, int[][] dp){
+    //     if (k == 0)
+    //         return true;
+
+    //     if (idx == 0)
+    //         return nums[0] == k;
+
+    //     if (dp[idx][k] != -1)
+    //         return dp[idx][k] == 0 ? false : true;
+
+    //     boolean notTaken = solve(idx - 1, k, nums, dp);
+
+    //     boolean taken = false;
+
+    //     if (nums[idx] <= k)
+    //         taken = solve(idx - 1, k - nums[idx], nums, dp);
+
+    //     dp[idx][k] = notTaken || taken ? 1 : 0;
+    //     return notTaken || taken;
+    // }
+
+    Boolean canPartition(Boolean[] memo, int s, int index, int[] nums) {
+        if (s == 0)
+            return true;
+
+        if (s < 0)
+            return false;
+       
+        if (index == 0)
+            return s == nums[0];
+       
+        if (memo[s] != null)
+            return memo[s];
+       
+        return memo[s] = canPartition(memo, s - nums[index], index - 1, nums) || canPartition(memo, s, index - 1, nums);
+    }
+
     public boolean canPartition(int[] nums) {
-        int sum = 0;
+
+        int s = 0;
+
+        for (int i = 0; i < nums.length; i++)
+            s += nums[i];
+
+        if (s % 2 != 0)
+            return false;
+
         int n = nums.length;
-        
-        for(int i : nums) sum+=i;
-        
-        if(sum%2!=0) return false;
-        
-        sum /= 2;
-        
-        boolean dp[][] = new boolean[n+1][sum+1];
-        
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=sum;j++){
-                if(i==0 || j==0)
-                    dp[i][j] = false;
-                else if(nums[i-1] > j)     // if curr sum value is greater than the current element value then just skip(take previous value)
-                    dp[i][j] = dp[i-1][j];
-                else if(nums[i-1]==j)  // we got required sum
-                    dp[i][j] = true;
-                else
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
-            }
-        }
-        return dp[n][sum];
-        
-        
+
+        s /= 2;
+
+        Boolean[] memo = new Boolean[s + 1];
+
+        return canPartition(memo, s, n - 1, nums);
+
+        // int sum = 0;
+        // int n = nums.length;
+
+        // for(int i : nums){
+        //     sum += i;
+        // }
+
+        // if(sum % 2 == 1){
+        //     return false;
+        // }else{
+        //     int k = sum / 2;
+
+        //     int dp[][] = new int[n][k + 1];
+
+        //     for (int row[] : dp)
+        //         Arrays.fill(row, -1);
+
+        //     return solve(n - 1, k, nums, dp);
+        // }
+
     }
 }
