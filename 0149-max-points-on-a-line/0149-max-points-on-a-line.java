19cae58ff386a834e768265e6177b8205107c33d
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-04-09 19:53:50 +0530

    Time: 7 ms (99.76%), Space: 44.6 MB (64.49%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,67 +1,31 @@
-  /*
-     *  A line is determined by two factors,say y=ax+b
-     *  
-     *  If two points(x1,y1) (x2,y2) are on the same line(Of course). 
+class Solution {
+ public int maxPoints(int[][] points) {
+        int len = points.length;
+        if (len < 3)
+            return len;
 
-     *  Consider the gap between two points.
+        int maxPoints = 2;
+        for (int i=0; i<len; i++)
+            maxPoints = Math.max(maxPoints, getMaxLine(points[i][0], points[i][1], i+1, points));
 
-     *  We have (y2-y1)=a(x2-x1),a=(y2-y1)/(x2-x1) a is a rational, b is canceled since b is a constant
-
-     *  If a third point (x3,y3) are on the same line. So we must have y3=ax3+b
-
-     *  Thus,(y3-y1)/(x3-x1)=(y2-y1)/(x2-x1)=a
-
-     *  Since a is a rational, there exists y0 and x0, y0/x0=(y3-y1)/(x3-x1)=(y2-y1)/(x2-x1)=a
-
-     *  So we can use y0&x0 to track a line;
-     */
-    
-    public class Solution{
-        public int maxPoints(Point[] points) {
-        	if (points==null) return 0;
-        	if (points.length<=2) return points.length;
-        	
-        	Map<Integer,Map<Integer,Integer>> map = new HashMap<Integer,Map<Integer,Integer>>();
-        	int result=0;
-        	for (int i=0;i<points.length;i++){ 
-        		map.clear();
-        		int overlap=0,max=0;
-        		for (int j=i+1;j<points.length;j++){
-        			int x=points[j].x-points[i].x;
-        			int y=points[j].y-points[i].y;
-        			if (x==0&&y==0){
-        				overlap++;
-        				continue;
-        			}
-        			int gcd=generateGCD(x,y);
-        			if (gcd!=0){
-        				x/=gcd;
-        				y/=gcd;
-        			}
-        			
-        			if (map.containsKey(x)){
-        				if (map.get(x).containsKey(y)){
-        					map.get(x).put(y, map.get(x).get(y)+1);
-        				}else{
-        					map.get(x).put(y, 1);
-        				}   					
-        			}else{
-        				Map<Integer,Integer> m = new HashMap<Integer,Integer>();
-        				m.put(y, 1);
-        				map.put(x, m);
-        			}
-        			max=Math.max(max, map.get(x).get(y));
-        		}
-        		result=Math.max(result, max+overlap+1);
-        	}
-        	return result;
-        	
-        	
-        }
-        private int generateGCD(int a,int b){
-    
-        	if (b==0) return a;
-        	else return generateGCD(b,a%b);
-        	
-        }
+        return maxPoints;
     }
+
+    private int getMaxLine(int x1, int y1, int j, int[][] points)  {
+        int len = points.length;
+        Map<Double, Integer> map = new HashMap<>();
+        int max = 1;
+        for (; j<len; j++)  {
+            int x2 = points[j][0], y2 = points[j][1];
+            //NEGATIVE_INFINITY or POSITIVE_INFINITY both work, just has to be same everytime
+            Double slope = (x2 == x1) ? Double.NEGATIVE_INFINITY : (double)(y2-y1)/(x2-x1);//((y2 == y1) ? 0.0 :
+            if(slope == -0.0){
+                slope = 0.0;
+            }
+            int count = map.getOrDefault(slope, 1)+1;
+            map.put(slope, count);
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+}
