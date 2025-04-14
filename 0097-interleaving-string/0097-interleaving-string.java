aeace92a33ab524c60ccb3f1f156eade1d5cee9b
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-04-14 08:22:58 +0530

    Time: 1 ms (90%), Space: 41.9 MB (58.07%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,22 +1,67 @@
-public class Solution {
+class Solution {
+
+    String s1;
+    String s2;
+    String s3;
+
+    Boolean[][] memo;
+
     public boolean isInterleave(String s1, String s2, String s3) {
-        int m = s1.length(), n = s2.length(), l = s3.length();
-        if (m + n != l) return false;
 
-        boolean[] dp = new boolean[n + 1];
-        dp[0] = true;
-
-        for (int j = 1; j <= n; ++j) {
-            dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        if(s3.length()!=(s1.length()+s2.length())){
+            return false;
         }
 
-        for (int i = 1; i <= m; ++i) {
-            dp[0] = dp[0] && s1.charAt(i - 1) == s3.charAt(i - 1);
-            for (int j = 1; j <= n; ++j) {
-                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+        this.s1=s1;
+        this.s2=s2;
+        this.s3=s3;
+        this.memo = new Boolean[s1.length()+1][s2.length()+1];
+
+        return findMatch(0,0);
+
+        // int s1Index =0;
+        // int s2Index = 0;
+
+        // for(int i=0;i<s3.length();i++){
+        //     System.out.println("s3="+s3.charAt(i)+" s1="+s1Index+" s2="+s2Index);
+        //     if(s1Index<s1.length() && s3.charAt(i)==s1.charAt(s1Index)){
+        //         s1Index++;
+        //     }else if(s2Index<s2.length() && s3.charAt(i)==s2.charAt(s2Index)){
+        //         s2Index++;
+        //     }else{
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+    }
+
+
+    public boolean findMatch(int s1Index,int s2Index){
+        if((s1Index+s2Index)>=s3.length()){
+            return true;
+        }
+
+        if(memo[s1Index][s2Index]!=null){
+            return memo[s1Index][s2Index];
+        }
+
+        if(s1Index<s1.length() && s3.charAt((s1Index+s2Index))==s1.charAt(s1Index)){
+            if(findMatch(s1Index+1,s2Index)){
+                memo[s1Index][s2Index] = true;
+                return true;
             }
         }
         
-        return dp[n];
+        if(s2Index<s2.length() && s3.charAt((s1Index+s2Index))==s2.charAt(s2Index)){
+            if(findMatch(s1Index,s2Index+1)){
+                memo[s1Index][s2Index] = true;
+                return true;
+            } 
+        }
+
+        memo[s1Index][s2Index] = false;
+        return false;
     }
 }
