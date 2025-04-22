67f5e680bf52d70e0b07ea611b278db620c0770e
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-04-22 22:10:36 +0530

    Time: 8 ms (97.73%), Space: 40.9 MB (97.73%) - LeetHub

--- 2415-count-the-number-of-ideal-arrays/2415-count-the-number-of-ideal-arrays.java
+++ 2415-count-the-number-of-ideal-arrays/2415-count-the-number-of-ideal-arrays.java
@@ -1,33 +1,43 @@
+import java.math.BigInteger;
+
 class Solution {
-    int MOD = 1000000007;
-    int[][] cnt = new int[10001][14];
-    int[][] comb = new int[10001][14];
+	public static final int MODULO = (int) 1e9 + 7;
 
-    public Solution() {
-        for (int s = 0; s < 10001; s++) {
-            for (int r = 0; r <= Math.min(s, 13); r++) {
-                comb[s][r] = (r == 0 || r == s) ? 1 : (comb[s - 1][r - 1] + comb[s - 1][r]) % MOD;
-            }
-        }
-
-        for (int div = 1; div < 10001; div++) {
-            cnt[div][0]++;
-            for (int i = div * 2; i < 10001; i += div) {
-                for (int bars = 0; bars < 13; bars++) {
-                    if (cnt[div][bars] > 0)
-                        cnt[i][bars + 1] += cnt[div][bars];
-                }
-            }
-        }
-    }
-
-    public int idealArrays(int n, int maxValue) {
-        int res = 0;
-        for (int i = 1; i <= maxValue; i++) {
-            for (int bars = 0; bars < Math.min(14, n); bars++) {
-                res = (int)((res + 1L * cnt[i][bars] * comb[n - 1][bars]) % MOD);
-            }
-        }
-        return res;
-    }
+	public static int idealArrays(int n, int maxValue) {
+		int[] minDivisor = new int[maxValue + 1];
+		for (int p = 2; p <= maxValue; p++) {
+			if (minDivisor[p] != 0)
+				continue;
+			for (int i = p; i <= maxValue; i += p)
+				if (minDivisor[i] == 0)
+					minDivisor[i] = p;
+		}
+		
+		int maxPow = (int) (Math.log(maxValue) / Math.log(2));
+		int[] binCoeff = new int[maxPow + 1];
+		BigInteger b = BigInteger.ONE;
+		BigInteger bigMod = BigInteger.valueOf(MODULO);
+		for (int i = 1; i <= maxPow; i++) {
+			b = b.multiply(BigInteger.valueOf(n + i - 1));
+			b = b.divide(BigInteger.valueOf(i));
+			binCoeff[i] = b.mod(bigMod).intValue();
+		}
+		
+		int s = 0;
+		for (int i = 1; i <= maxValue; i++) {
+			int x = i;
+			long prodBin = 1;
+			while (x > 1) {
+				int p = minDivisor[x];
+				int w = 0;
+				do {
+					w++;
+					x /= p;
+				} while (x % p == 0);
+				prodBin = prodBin * binCoeff[w] % MODULO;
+			}
+			s = (s + (int) prodBin) % MODULO;
+		}
+		return s;
+	}
 }
