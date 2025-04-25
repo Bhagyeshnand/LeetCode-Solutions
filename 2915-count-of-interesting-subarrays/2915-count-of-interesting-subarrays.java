ffa946bbbdab3dec2006b11112bbe76ba96a1ea2
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-04-25 11:09:00 +0530

    Time: 8 ms (97.25%), Space: 56.9 MB (86.24%) - LeetHub

--- 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.java
+++ 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.java
@@ -1,15 +1,28 @@
 class Solution {
     public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
-        long count = 0, equals = 0;
-        Map<Integer, Long> mpp = new HashMap<>();
-        mpp.put(0, 1L);
-        for (int i : nums) {
-            if (i % modulo == k) equals++;
-            int rem = (int)(equals % modulo);
-            int needed = (rem - k + modulo) % modulo;
-            count += mpp.getOrDefault(needed, 0L);
-            mpp.put(rem, mpp.getOrDefault(rem, 0L) + 1);
+
+        int n = nums.size();
+        if(k > n) return 0;
+         
+        int[] count = new int[n + 1];
+        count[0] = 1;
+
+        long ans = 0;
+        int sum = 0;
+        for(int x:nums){
+            x %= modulo;
+            if(x == k)
+                 ++sum;
+
+            sum %= modulo;
+            int r = sum - k;
+            if(r < 0) r += modulo;
+            if(r < n)
+               ans += count[r];
+
+            count[sum]++;
         }
-        return count;
+        
+        return ans;
     }
 }
