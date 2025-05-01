ea5330c7868c09c65c2b8dc796a2916f9fab6db5
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-05-01 15:14:33 +0530

    Time: 1 ms (100%), Space: 42.7 MB (58.12%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.java
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.java
@@ -1,33 +1,18 @@
 class Solution {
     public int countPairs(int[] nums, int k) {
-        int pairs = 0;
-        Map<Integer, List<Integer>> mpp = new HashMap<>();
-        for (int i = 0; i < nums.length; i++)
-            mpp.computeIfAbsent(nums[i], x -> new ArrayList<>()).add(i);
-        
-        List<Integer> divisors = new ArrayList<>();
-        for (int d = 1; d * d <= k; d++) {
-            if (k % d == 0) {
-                divisors.add(d);
-                if (d != k / d) divisors.add(k / d);
-            }
-        }
-
-        for (Map.Entry<Integer, List<Integer>> entry : mpp.entrySet()) {
-            Map<Integer, Integer> mpp2 = new HashMap<>();
-            List<Integer> vec = entry.getValue();
-            for (int i : vec) {
-                int gcdd = gcd(i, k);
-                int need = k / gcdd;
-                pairs += mpp2.getOrDefault(need, 0);
-                for (int d : divisors)
-                    if (i % d == 0) mpp2.put(d, mpp2.getOrDefault(d, 0) + 1);
-            }
-        }
-        return pairs;
+        return helper(nums,k,0);
     }
 
-    private int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
+private static int helper(int[] nums, int k, int i){
+    if(i>=nums.length){
+        return 0;
     }
+    int count = 0;
+    for(int j=i+1;j<nums.length;j++){
+        if(nums[i]==nums[j] && (i*j)%k==0){
+            count++;
+        }
+    }
+    return count+helper(nums,k,i+1);
+  }
 }
