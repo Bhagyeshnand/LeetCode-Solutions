2221a86db8c41b375d17cc4b67360a9c316e8cba
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-05-13 21:02:02 +0530

    Time: 11 ms (100%), Space: 45.7 MB (41.77%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.java
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.java
@@ -1,26 +1,20 @@
 class Solution {
-    private static final int mod = 1000000007;
-    private int mod_add(int a, int b) {
-        a %= mod; b %= mod;
-        return ((a + b) % mod + mod) % mod;
-    }
     public int lengthAfterTransformations(String s, int t) {
-        int[] nums = new int[26];
-        for (char ch : s.toCharArray()) nums[ch - 'a']++;
-        while (t-- > 0) {
-            int[] cur = new int[26];
-            for (int j = 0; j < 26; j++) {
-                if (j == 25 && nums[j] > 0) {
-                    cur[0] = mod_add(cur[0], nums[j]);
-                    cur[1] = mod_add(cur[1], nums[j]);
-                } else {
-                    if (j < 25) cur[j + 1] = mod_add(cur[j + 1], nums[j]);
-                }
-            }
-            nums = cur;
+    	int MOD = (int)1e9 + 7, ans = 0;
+        long[] count = new long[26];
+        for (int c : s.toCharArray())
+        	count[c - 'a']++;
+        for (; t >= 26; t -= 26) {
+        	long z = count[25];
+        	for (int i = 25; i > 0; i--)
+        		count[i] = (count[i] + count[i - 1]) % MOD;
+        	count[0] = (count[0] + z) % MOD;
+        	count[1] = (count[1] + z) % MOD;
         }
-        int ans = 0;
-        for (int i : nums) ans = mod_add(ans, i);
+        for (int i = 0; i < 26; i++)
+        	ans = (int)((ans + count[i]) % MOD);
+        for (int i = 26 - t; i < 26; i++)
+        	ans = (int)((ans + count[i]) % MOD);
         return ans;
     }
 }
