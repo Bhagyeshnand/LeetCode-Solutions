864aefc409c883c180162119fd675e03bf6b2b42
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-06-13 12:06:40 +0530

    Time: 15 ms (99.5%), Space: 56.1 MB (63%) - LeetHub

--- 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.java
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.java
@@ -1,19 +1,31 @@
 class Solution {
     public int minimizeMax(int[] nums, int p) {
-        if (p == 0) return 0;
         Arrays.sort(nums);
-        int n = nums.length, left = 0, right = nums[n-1] - nums[0];
-        while (left < right) {
-            int mid = left + (right - left) / 2, pairs = 0;
-            for (int i = 1; i < n; i++) {
-                if (nums[i] - nums[i-1] <= mid) {
-                    pairs++;
-                    i++;
-                }
+        int left = 0, right = 0, n = nums.length;
+        for (int i = 1; i < n; i++) {
+            right = Math.max(right, nums[i] - nums[i - 1]);
+        }
+        while (left <= right) {
+            int mid = left + (right - left >> 1);
+            if (!check(nums, mid, p)) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-            if (pairs >= p) right = mid;
-            else left = mid + 1;
         }
         return left;
+    }
+    
+    private boolean check(int[] nums, int x, int p) {
+        int i = 1, n = nums.length;
+        while (i < n && p > 0) {
+            if (nums[i] - nums[i - 1] <= x) {
+                i += 2;
+                p--;
+            } else {
+                i++;
+            }
+        }
+        return p == 0;
     }
 }
