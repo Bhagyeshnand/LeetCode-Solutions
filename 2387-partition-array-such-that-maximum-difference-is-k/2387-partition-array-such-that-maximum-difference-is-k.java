8532dd7a37af44d99c6e2bdeafab676d80cd8d11
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-06-19 10:44:13 +0530

    Time: 3 ms (100%), Space: 60 MB (9.22%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.java
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.java
@@ -1,12 +1,44 @@
 class Solution {
     public int partitionArray(int[] nums, int k) {
-        Arrays.sort(nums);
-        int ans = 1;
-        int min = nums[0];
-        for (int i = 1;  i < nums.length; i++) {
-            if (nums[i] - min > k) {
+        // Arrays.sort(nums);
+        // int min=nums[0];
+        // int max=nums[0];
+        // int count=1;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]<min){
+        //         min=nums[i];
+        //     }
+        //     if(nums[i]>max){
+        //         max=nums[i];
+        //     }
+        //     if(Math.abs(min-max)>k){
+        //         count++;
+        //         min=nums[i];
+        //         max=nums[i];
+        //     }
+        // }
+        // return count;
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int num:nums){
+            if(num<min) min=num;
+            if(num>max) max=num;
+        }
+        if(max-min<=k) return 1;
+        int[] count=new int[max-min+1];
+        for(int x:nums){
+            ++count[x-min];
+        }
+        int ans=1;
+        int x=k+1;
+        max=max-min;
+        while(x<=max){
+            while(x<=max && count[x]==0){
+                x++;
+            }
+            if(x<=max){
                 ans++;
-                min = nums[i];
+                x+=k+1;
             }
         }
         return ans;
