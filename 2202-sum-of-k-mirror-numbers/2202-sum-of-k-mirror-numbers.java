eb0560c776956377c9a284ef69eb25f0eb5a607d
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-06-23 10:55:00 +0530

    Time: 93 ms (92.86%), Space: 44.2 MB (67.86%) - LeetHub

--- 2202-sum-of-k-mirror-numbers/2202-sum-of-k-mirror-numbers.java
+++ 2202-sum-of-k-mirror-numbers/2202-sum-of-k-mirror-numbers.java
@@ -1,39 +1,63 @@
 class Solution {
-
-    long createPalindrome(long num, boolean odd) {
-        long x = num;
-        if (odd) x /= 10;
-        while (x > 0) {
-            num = num * 10 + x % 10;
-            x /= 10;
-        }
-        return num;
-    }
-
-    boolean isPalindrome(long num, int base) {
-        StringBuilder sb = new StringBuilder();
-        while (num > 0) {
-            sb.append((char) (num % base + '0'));
-            num /= base;
-        }
-        String s = sb.toString();
-        int i = 0, j = s.length() - 1;
-        while (i < j) if (s.charAt(i++) != s.charAt(j--)) return false;
-        return true;
-    }
-
     public long kMirror(int k, int n) {
-        long sum = 0;
-        for (long len = 1; n > 0; len *= 10) {
-            for (long i = len; n > 0 && i < len * 10; i++) {
-                long p = createPalindrome(i, true);
-                if (isPalindrome(p, k)) { sum += p; n--; }
+        int[] remain = new int[]{n};
+        long r = 0;
+        int i = 1;
+        while (remain[0] > 0) {
+            r += calculate(k, remain, i);
+            i++;
+        }
+        return r;
+    }
+
+    private long calculate(int k, int[] remain, int d) {
+        long[] sum = new long[]{0};
+        search(new int[d], k, 0, true, remain, sum);
+        if (remain[0] > 0) {
+            search(new int[d], k, 0, false, remain, sum);
+        }
+        return sum[0];
+    }
+
+    private void search(int[] num, int k, int i, boolean odd, int[] remain, long[] sum) {
+        if (i >= num.length) {
+            int base = 1;
+            long v = 0;
+            for (int j = 0; j < num.length; j++) {
+                v = v * k + num[j];
             }
-            for (long i = len; n > 0 && i < len * 10; i++) {
-                long p = createPalindrome(i, false);
-                if (isPalindrome(p, k)) { sum += p; n--; }
+            for (int j = num.length-1; j >= 0; j--) {
+                if (odd && j == num.length-1) {
+                    continue;
+                }
+                v = v * k + num[j];
+            }
+            if (mirror(v)) {
+                // System.out.println(v);
+                remain[0]--;
+                sum[0] += v;
+            }
+            return;
+        }
+        for (int j = 0; j < k; j++) {
+            if (i == 0 && j == 0) {
+                continue;
+            }
+            num[i] = j;
+            search(num, k, i+1, odd, remain, sum);
+            if (remain[0] <= 0) {
+                break;
             }
         }
-        return sum;
+    }
+
+    private boolean mirror(long v) {
+        String s = String.valueOf(v);
+        for (int i = 0; i < s.length()/2; i++) {
+            if (s.charAt(i) != s.charAt(s.length()-i-1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
