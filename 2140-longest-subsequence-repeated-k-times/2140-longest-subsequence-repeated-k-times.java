f0fa43eed0b820e55df114f256a55f6f14cd33c0
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-06-27 23:01:38 +0530

    Time: 97 ms (100%), Space: 45.4 MB (45.28%) - LeetHub

--- 2140-longest-subsequence-repeated-k-times/2140-longest-subsequence-repeated-k-times.java
+++ 2140-longest-subsequence-repeated-k-times/2140-longest-subsequence-repeated-k-times.java
@@ -1,29 +1,43 @@
 class Solution {
     public String longestSubsequenceRepeatedK(String s, int k) {
-        String r="";
-        Queue<String> q=new LinkedList<>();
-        for(q.add("");!q.isEmpty();) {
-            String c=q.poll();
-            for(char ch='a';ch<='z';ch++) {
-                String n=c+ch;
-                if(isK(n,s,k)) {
-                    r=n;
-                    q.add(n);
+        char[] ca= s.toCharArray();
+        int n= ca.length;
+        
+        char[] freq= new char[26];
+        for(int i=0; i<n; i++)
+            ++freq[ca[i]-'a'];
+
+        // all candidate subsequences grouped by length - max subsequence length==7
+        ArrayList<String>[] cand= new ArrayList[8];
+        cand[1]= new ArrayList<>();
+        String ans= "";
+        for(int i=0; i<26; i++)
+            if(freq[i]>=k)
+                cand[1].add(ans= "" + (char)('a'+i));
+        // the candidate subsequence can start with any of characters with freq>=k
+        for(int i=2; i<8; i++){
+            cand[i]= new ArrayList<>();
+            for(String prev:cand[i-1])
+                for(String c:cand[1]){
+                    String next= prev+c;
+                    if(isSubsequenceRepeatedK(ca, next, k))
+                        cand[i].add(ans= next);
                 }
-            }
         }
-        return r;
+        return ans;
     }
-    boolean isK(String s,String t,int k) {
-        int c=0,i=0;
-        for(char ch:t.toCharArray()) {
-            if(ch==s.charAt(i)) {
-                if(++i==s.length()) {
-                    i=0;
-                    if(++c==k) return true;
-                }
+    
+    boolean isSubsequenceRepeatedK(char[] ca, String t, int k){
+        char[] ta= t.toCharArray();
+        int n= ca.length, m= ta.length, i= 0;
+        while(k-->0){
+            int j=0;
+            while(i<n && j<m){
+                if(ca[i]==ta[j]) j++;
+                i++;
             }
+            if(j!=m) return false;
         }
-        return false;
+        return true;
     }
 }
