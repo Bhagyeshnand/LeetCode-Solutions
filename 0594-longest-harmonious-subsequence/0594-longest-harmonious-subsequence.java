87962be59d5d8ff3e65084ea4e5c2e846ea2485a
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-06-30 10:27:22 +0530

    Time: 6 ms (100%), Space: 45.7 MB (48.6%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -1,21 +1,41 @@
-public class Solution {
-    public int findLHS(int[] nums) {
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        
-        // Step 1: Count frequencies of each number
-        for (int num : nums) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+class Solution {
+    static{
+        for (int i = 0; i < 500; i++){
+            findLHS(new int[]{1,2,3,4});
         }
-        
-        int maxLength = 0;
-        
-        // Step 2: Check all possible (num, num+1) pairs
-        for (int num : frequencyMap.keySet()) {
-            if (frequencyMap.containsKey(num + 1)) {
-                int currentLength = frequencyMap.get(num) + frequencyMap.get(num + 1);
-                maxLength = Math.max(maxLength, currentLength);
+    }
+    public static int findLHS(int[] nums) {
+        int minValue = Integer.MAX_VALUE;
+        int maxValue = Integer.MIN_VALUE;
+        for (int num : nums) {
+            minValue = Math.min(minValue, num);
+            maxValue = Math.max(maxValue, num);
+        }
+        int size = maxValue - minValue + 1;
+        if (size > 1000000) return findLHSUsingMap(nums);
+        int[] valueToCount = new int[size];
+        for (int num : nums) valueToCount[num - minValue]++;
+        int answer = 0;
+        for (int i = 1; i < valueToCount.length; i++) {
+            if (valueToCount[i] != 0 && valueToCount[i - 1] != 0) {
+                answer = Math.max(answer, valueToCount[i] + valueToCount[i - 1]);
             }
         }
-        return maxLength;
+        return answer;
+    }
+    private static int findLHSUsingMap(int[] nums) {
+        if (nums.length < 2) return 0;
+        Map<Integer, Integer> valueToCount = new HashMap();
+        for (int num : nums) valueToCount.merge(num, 1, Integer::sum);
+        int answer = 0;
+        for (Map.Entry<Integer,Integer> entry : valueToCount.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            Integer less = valueToCount.get(key - 1);
+            Integer more = valueToCount.get(key + 1);
+            if (less != null) answer = Math.max(answer, value + less);
+            if (more != null) answer = Math.max(answer, value + more);
+        }
+        return answer;
     }
 }
