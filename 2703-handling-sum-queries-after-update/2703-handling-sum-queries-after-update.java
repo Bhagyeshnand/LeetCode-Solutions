2d62bda27507445f8ed506485b18f50e81952c39
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-07-01 13:31:53 +0530

    Time: 24 ms (100%), Space: 78.1 MB (92.31%) - LeetHub

--- 2703-handling-sum-queries-after-update/2703-handling-sum-queries-after-update.java
+++ 2703-handling-sum-queries-after-update/2703-handling-sum-queries-after-update.java
@@ -1,26 +1,89 @@
 class Solution {
+
+    private class SegT {
+        int n;
+        int seg[];
+        int lazy[];
+
+        public SegT(int arr[]) {
+            n = arr.length;
+            seg = new int[4*n];
+            lazy = new int[4*n];
+
+            build(arr, 0, n-1, 0);
+        }
+
+        private int build(int arr[], int l, int r, int si) {
+            if(l == r) {
+                return seg[si] = arr[l];
+            }
+            int mid = l + (r-l)/2;
+            return seg[si] = build(arr, l, mid, 2*si+1) + build(arr, mid+1, r, 2*si+2);
+        }
+
+        private void update(int L, int R, int l, int r, int si) {
+            if(lazy[si] != 0) {
+                seg[si] = r - l + 1 - seg[si];
+                if(l != r) {
+                    lazy[2*si+1] = 1 - lazy[2*si+1];
+                    lazy[2*si+2] = 1 - lazy[2*si+2];
+                }
+                lazy[si] = 0;
+            }
+
+            // No overlap
+            if(l>R || r<L) {
+                return;
+            }
+            // Complete overlap
+            if(l>=L && r<=R) {
+                seg[si] = r - l + 1 - seg[si];
+                if(l != r) {
+                    lazy[2*si+1] = 1 - lazy[2*si+1];
+                    lazy[2*si+2] = 1 - lazy[2*si+2];
+                }
+                return;
+            }
+
+            // Partial overlap
+            int mid = l +(r-l)/2;
+            update(L, R, l, mid, 2*si+1);
+            update(L, R, mid+1, r, 2*si+2);
+            seg[si] = seg[2*si+1] + seg[2*si+2];
+        }
+
+        public void update(int L, int R) {
+            update(L, R, 0, n-1, 0);
+        }
+
+        public int query() {
+            return seg[0];
+        }
+    }
+
     public long[] handleQuery(int[] nums1, int[] nums2, int[][] queries) {
-        List<Long> ls = new ArrayList<>();
-        int n = nums1.length;
-        BitSet bs = new BitSet(n);
+
         long sum = 0;
-        for(int i = 0;i < n;i++){
-            sum += 1L * nums2[i];
-            if(nums1[i] == 1) bs.set(i);
+        for(int i: nums2) {
+            sum += i;
         }
-        for(var q:queries){
-            if(q[0] == 1){
-                bs.flip(q[1],q[2] + 1);
+
+        SegT sgt = new SegT(nums1);
+        List<Long> list = new ArrayList<>();
+        for(int ar[]: queries) {
+            if(ar[0] == 1) {
+                sgt.update(ar[1], ar[2]);
+            } else if(ar[0] == 2) {
+                sum += (long)ar[1] * sgt.query();
+            } else {
+                list.add(sum);
             }
-            else if(q[0] == 2){
-                sum += 1L * q[1] * bs.cardinality();
-            }
-            else ls.add(sum);
         }
-        long ans[] = new long[ls.size()];
-        for(int i = 0;i < ans.length;i++){
-            ans[i] = ls.get(i);
+        long ans[] = new long[list.size()];
+        for(int i=0;i<ans.length;i++) {
+            ans[i] = list.get(i);
         }
+
         return ans;
     }
 }
