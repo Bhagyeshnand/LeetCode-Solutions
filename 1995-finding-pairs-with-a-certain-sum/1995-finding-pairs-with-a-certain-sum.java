54f5db921bf1b85e422114285f76c3dfe433215f
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-07-06 11:50:36 +0530

    Time: 117 ms (99.65%), Space: 78.6 MB (25.61%) - LeetHub

--- 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.java
@@ -1,22 +1,63 @@
 class FindSumPairs {
-    int[] n1, n2;
-    Map<Integer, Integer> m = new HashMap<>();
+    int[] nums1;
+    int[] nums2;
+
+    // freq maps
+    Map<Integer,Integer> mp1;
+    Map<Integer,Integer> mp2;
+    // sorted distinct keys from nums1
+    List<Integer> sortedKeys1;
 
     public FindSumPairs(int[] nums1, int[] nums2) {
-        n1 = nums1;
-        n2 = nums2;
-        for (int x : n2) m.put(x, m.getOrDefault(x, 0) + 1);
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+
+        mp1 = new HashMap<>();
+        mp2 = new HashMap<>();
+
+        // build freq map for nums1
+        for (int x : nums1) {
+            mp1.put(x, mp1.getOrDefault(x, 0) + 1);
+        }
+        // cache & sort the distinct nums1 keys
+        sortedKeys1 = new ArrayList<>(mp1.keySet());
+        Collections.sort(sortedKeys1);
+
+        // build freq map for nums2
+        for (int x : nums2) {
+            mp2.put(x, mp2.getOrDefault(x, 0) + 1);
+        }
     }
 
-    public void add(int i, int v) {
-        m.put(n2[i], m.get(n2[i]) - 1);
-        n2[i] += v;
-        m.put(n2[i], m.getOrDefault(n2[i], 0) + 1);
+    public void add(int index, int val) {
+        int oldVal = nums2[index];
+        int newVal = oldVal + val;
+
+        // decrement or remove oldVal
+        int countOld = mp2.get(oldVal);
+        if (countOld == 1) {
+            mp2.remove(oldVal);
+        } else {
+            mp2.put(oldVal, countOld - 1);
+        }
+
+        // increment newVal
+        mp2.put(newVal, mp2.getOrDefault(newVal, 0) + 1);
+
+        // commit it to the array
+        nums2[index] = newVal;
     }
 
-    public int count(int t) {
-        int c = 0;
-        for (int x : n1) c += m.getOrDefault(t - x, 0);
-        return c;
+    public int count(int tot) {
+        int res = 0;
+        // iterate sorted keys; break as soon as key > tot
+        for (int k : sortedKeys1) {
+            if (k > tot) break; 
+            Integer c2 = mp2.get(tot - k);
+            if (c2 != null) {
+                res += mp1.get(k) * c2;
+            }
+        }
+        return res;
     }
 }
