6d7f6e1385016c6e7ae84d48a1c44dc554f0dc4b
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-07-14 14:22:03 +0530

    Time: 4 ms (96.41%), Space: 46.3 MB (6.67%) - LeetHub

--- 1239-largest-1-bordered-square/1239-largest-1-bordered-square.java
+++ 1239-largest-1-bordered-square/1239-largest-1-bordered-square.java
@@ -1,23 +1,36 @@
 class Solution {
-    public int largest1BorderedSquare(int[][] A) {
-        int m = A.length, n = A[0].length;
-        int[][] left = new int[m][n], top = new int[m][n];
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (A[i][j] > 0) {
-                    left[i][j] = j > 0 ? left[i][j - 1] + 1 : 1;
-                    top[i][j] = i > 0  ? top[i - 1][j] + 1 : 1;
+    public int largest1BorderedSquare(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] hor = new int[m][n]; 
+        int[][] ver = new int[m][n];  
+        int max = 0;
+
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    hor[i][j] = (j == 0 ? 0 : hor[i][j - 1]) + 1;
+                    ver[i][j] = (i == 0 ? 0 : ver[i - 1][j]) + 1;
                 }
             }
         }
-        for (int l = Math.min(m, n); l > 0; --l)
-            for (int i = 0; i < m - l + 1; ++i)
-                for (int j = 0; j < n - l + 1; ++j)
-                    if (top[i + l - 1][j] >= l &&
-                            top[i + l - 1][j + l - 1] >= l &&
-                            left[i][j + l - 1] >= l &&
-                            left[i + l - 1][j + l - 1] >= l)
-                        return l * l;
-        return 0;
+
+        
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int small = Math.min(hor[i][j], ver[i][j]);
+
+                while (small > max) {
+                     
+                     
+                    if (ver[i][j - small + 1] >= small && hor[i - small + 1][j] >= small) {
+                        max = small;
+                    }
+                    small--;
+                }
+            }
+        }
+
+        return max * max;  
     }
 }
