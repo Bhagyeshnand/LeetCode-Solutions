80f35f069f8995ff9950b195dbac5f936da3b765
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-07-15 09:02:17 +0530

    Time: 1 ms (99.12%), Space: 41.9 MB (90.73%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -1,24 +1,28 @@
-public class Solution {
-    public boolean isValid(String s) {
-        int n = s.length();
-        if (n < 3) {
+class Solution {
+    public boolean isValid(String word) {
+        if(word.length() < 3) {
             return false;
         }
 
-        int vowels = 0;
-        int consonants = 0;
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
 
-        for (char c : s.toCharArray()) {
-            if (Character.isLetter(c)) {
-                if ("aeiouAEIOU".indexOf(c) != -1) {
-                    vowels++;
-                } else {
-                    consonants++;
-                }
-            } else if (!Character.isDigit(c)) {
-                return false;
+        for(int i = 0; i < word.length(); i++) {
+            switch(word.charAt(i)) {
+                case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+                    hasVowel = true;
+                    break;
+                case 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z',
+                     'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z':
+                    hasConsonant = true;
+                    break;
+                case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+                    break;
+                default:
+                    return false;
             }
         }
-        return vowels >= 1 && consonants >= 1;
+
+        return hasVowel && hasConsonant;
     }
 }
