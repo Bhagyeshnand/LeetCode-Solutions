1c2c1f23f8ba229129461e550eec033648a043cb
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-07-21 11:05:06 +0530

    Time: 14 ms (100%), Space: 45.7 MB (78.54%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,18 +1,22 @@
+//\U0001d4df\U0001d4f5\U0001d4ee\U0001d4ea\U0001d4fc\U0001d4ee \U0001d4e4\U0001d4df\U0001d4e5\U0001d4de\U0001d4e3\U0001d4d4 \U0001d4f2\U0001d4ef \U0001d4f2\U0001d4fd \U0001d4f1\U0001d4ee\U0001d4f5\U0001d4f9\U0001d4fc\U0001f44d\U0001f3fb
 class Solution {
     public String makeFancyString(String s) {
-        int sameCount = 0;
-        StringBuilder sb = new StringBuilder();
-        char prev = s.charAt(0);
-        for (char cur : s.toCharArray()) {
-            if (cur == prev) {
-                sameCount++;
+        char[] chars = s.toCharArray();
+        char last = chars[0];
+        int count = 1;
+        int pos = 1;
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] != last) {
+                last = chars[i];
+                count = 0;
             }
-            else {
-                sameCount = 1;
-            }
-            if (sameCount < 3) sb.append(cur);
-            prev = cur;
+
+            if (++count > 2) continue;
+
+            chars[pos++] = chars[i];
         }
-        return sb.toString();
+
+        return new String(chars, 0, pos);
     }
 }
