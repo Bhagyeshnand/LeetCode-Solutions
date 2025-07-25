d0bcba3799e3877ba5c781d20aab123d8fa82082
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-07-25 18:32:00 +0530

    Time: 1 ms (100%), Space: 42.6 MB (39.38%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -1,24 +1,17 @@
 class Solution {
     public int maxSum(int[] nums) {
-        // 1. Sort the nums array in ascending order.
         Arrays.sort(nums);
-
-        // 2. Initialize the variables:
-        // - sum: The current amount (starting with the largest element).
-        int sum = nums[nums.length - 1];
-        // - prev: The previous added element (starting with the largest element).
-        int prev = sum;
-
-        // 3. A loop running from the end of the array to the beginning, as long as two conditions are met:
-        // a) i >= 0 (we do not go beyond the boundaries of the array).
-        // b) nums[i] >= 0 (we continue only if the current element is >= 0).
-        for (int i = nums.length - 2; i >= 0 && nums[i] >= 0; prev = nums[i], i--)
-// 4. If the current element (nums[i]) differs from the previous one (prev),
-// then add it to the amount.
-            if (nums[i] != prev)
-                sum += nums[i];
-
-        // 5. We return the accumulated amount.
+        int prev = nums[nums.length - 1];
+        int sum = prev;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int n = nums[i];
+            if (n <= 0) {
+                return sum;
+            } else if (n != prev) {
+                sum = sum + n;
+            }
+            prev = n;
+        }
         return sum;
     }
 }
