9009e9c649a002dd4dd2a66b1cea85abdb4a1182
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-08-11 21:12:28 +0530

    Time: 3 ms (100%), Space: 93.5 MB (16.13%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.java
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.java
@@ -1,26 +1,30 @@
 class Solution {
     public int[] productQueries(int n, int[][] queries) {
-        final int MOD = 1000000007;
+        int power = 1, MOD = (int) 1e9 + 7;
+        while (power <= n) power <<= 1;
+        power >>= 1;
+
         List<Integer> powers = new ArrayList<>();
         while (n > 0) {
-            int lowBit = n & -n;
-            powers.add(lowBit);
-            n ^= lowBit;
-        }
-        int size = powers.size();
-        int[][] table = new int[size][size];
-        for (int row = 0; row < size; row++) {
-            table[row][row] = powers.get(row);
-            for (int col = row + 1; col < size; col++) {
-                table[row][col] = (int)((long)table[row][col - 1] * powers.get(col) % MOD);
+            if (power <= n) {
+                powers.add(power);
+                n -= power;
             }
+            power >>= 1;
         }
-        int[] result = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            int p = queries[i][0];
-            int q = queries[i][1];
-            result[i] = table[p][q];
+
+        n = powers.size();
+        int[][] prefix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            prefix[i][i] = powers.get(n - 1 - i);
+            for (int j = i + 1; j < n; j++)
+                prefix[i][j] = (int)((1L * prefix[i][j - 1] * powers.get(n - 1 - j)) % MOD);
         }
-        return result;
+
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++)
+            res[i] = prefix[queries[i][0]][queries[i][1]];
+
+        return res;
     }
 }
