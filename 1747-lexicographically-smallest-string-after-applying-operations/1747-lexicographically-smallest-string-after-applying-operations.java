5609bf4ff46ead2f6bc2c48b1bddbfa429a21efc
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-10-19 23:07:03 +0530

    Time: 1 ms (100%), Space: 41.6 MB (100%) - LeetHub

--- 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.java
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.java
@@ -1,41 +1,38 @@
+// https://www.youtube.com/@0x3f
 class Solution {
     public String findLexSmallestString(String S, int a, int b) {
         char[] s = S.toCharArray();
         int n = s.length;
+        char[] t = new char[n];
         int step = gcd(b, n);
         int g = gcd(a, 10);
-        String ans = S;
+        String ans = null;
 
         for (int i = 0; i < n; i += step) {
-            char[] t = rotate(s, i, n);
-            
-            optimizeDigits(t, 1, g);
-            
-            if (step % 2 == 1) {
-                optimizeDigits(t, 0, g);
+            // t = s[i,n) + s[0,i)
+            System.arraycopy(s, i, t, 0, n - i);
+            System.arraycopy(s, 0, t, n - i, i);
+
+            modify(t, 1, g); // 累加操作（所有奇数下标）
+            if (step % 2 > 0) { // 能对偶数下标执行累加操作
+                modify(t, 0, g); // 累加操作（所有偶数下标）
             }
 
-            String candidate = new String(t);
-            if (candidate.compareTo(ans) < 0) {
-                ans = candidate;
+            String str = new String(t);
+            if (ans == null || str.compareTo(ans) < 0) {
+                ans = str;
             }
         }
 
         return ans;
     }
 
-    private char[] rotate(char[] s, int start, int n) {
-        char[] rotated = new char[n];
-        System.arraycopy(s, start, rotated, 0, n - start);
-        System.arraycopy(s, 0, rotated, n - start, start);
-        return rotated;
-    }
-
-    private void optimizeDigits(char[] t, int start, int g) {
-        int firstDigit = t[start] - '0';
-        int minValue = firstDigit % g;
-        int inc = (minValue - firstDigit + 10) % 10;
-        
+    private void modify(char[] t, int start, int g) {
+        int ch = t[start] - '0'; // 最靠前的数字，越小越好
+        // ch 可以变成的最小值为 ch%g
+        // 例如 ch=5，g=2，那么 ch+2+2+2（模 10）后变成 1，不可能变得更小
+        // 从 ch 到 ch%g，需要增加 inc，其中 +10 保证 inc 非负（循环中会 %10 保证结果在 [0,9] 中）
+        int inc = ch % g - ch + 10;
         for (int j = start; j < t.length; j += 2) {
             t[j] = (char) ('0' + (t[j] - '0' + inc) % 10);
         }
@@ -43,9 +40,9 @@ class Solution {
 
     private int gcd(int a, int b) {
         while (a != 0) {
-            int temp = a;
+            int tmp = a;
             a = b % a;
-            b = temp;
+            b = tmp;
         }
         return b;
     }
