cc195d2ffa3d05a6198e2a5966636db464499670
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-10-25 13:06:48 +0530

    Time: 0 ms (100%), Space: 40.3 MB (90.33%) - LeetHub

---
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -0,0 +1,12 @@
+class Solution {
+    int triSum(int n) {
+        return (n * (n + 1)) >> 1;
+    }
+
+    public int totalMoney(int days) {
+        int nWeeks = days / 7;
+        int rDays = days % 7;
+
+        return triSum(days) - 42 * triSum(nWeeks - 1) - 6 * nWeeks * rDays;
+    }
+}
