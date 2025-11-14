52b7e6a9fb2839babfb4706ce36fe31d340de6b5
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-11-15 00:16:17 +0530

    Time: 7 ms (100%), Space: 77 MB (21.05%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.java
@@ -1,25 +1,22 @@
 class Solution {
-    public int[][] rangeAddQueries(int n, int[][] queries) {
-        int[][] diff = new int[n + 1][n + 1];
-        
-        for (int[] q : queries) {
-            int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
-            diff[r1][c1]++;
-            diff[r2 + 1][c1]--;
-            diff[r1][c2 + 1]--;
-            diff[r2 + 1][c2 + 1]++;
-        }
-        
-        int[][] mat = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int above = i > 0 ? mat[i - 1][j] : 0;
-                int left = j > 0 ? mat[i][j - 1] : 0;
-                int diag = i > 0 && j > 0 ? mat[i - 1][j - 1] : 0;
-                mat[i][j] = diff[i][j] + above + left - diag;
+    public int[][] rangeAddQueries(int n, int[][] Q) {
+        int[][] res = new int[n][n];
+        for (var q : Q) {
+            int r0 = q[0], c0 = q[1], r1 = q[2]+1, c1 = q[3]+1;
+            res[r0][c0]++;
+            if (c1 < n) res[r0][c1]--;
+            if (r1 < n) {
+                res[r1][c0]--;
+                if (c1 < n) res[r1][c1]++;
             }
         }
-        
-        return mat;
+
+        for (int i = 0; i < n; i++) for (int j = 1; j < n; j++)
+            res[i][j] += res[i][j-1];
+
+        for (int i = 1; i < n; i++) for (int j = 0; j < n; j++)
+            res[i][j] += res[i-1][j];
+
+        return res;
     }
 }
