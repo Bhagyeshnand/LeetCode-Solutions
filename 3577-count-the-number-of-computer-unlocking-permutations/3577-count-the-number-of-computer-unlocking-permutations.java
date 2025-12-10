eb332fe2190149e5f67d7e1ced80f8c7a6e2f444
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-12-10 18:10:32 +0530

    Time: 1 ms (100%), Space: 92.2 MB (13.33%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.java
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.java
@@ -4,13 +4,10 @@ class Solution {
     public int countPermutations(int[] comp) {
         int n = comp.length;
         int first = comp[0];
-
-        // Check that first is the unique minimum
         for (int i = 1; i < n; i++) {
             if (comp[i] <= first) return 0;
         }
 
-        // Compute factorial (n-1)!
         long fact = 1;
         for (int i = 2; i < n; i++) {
             fact = (fact * i) % MOD;
