de8743eead6c43b65339d918a40cfdeee2507596
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-12-12 19:17:10 +0530

    Time: 9 ms (92.8%), Space: 47.7 MB (28%) - LeetHub

--- 3433-count-mentions-per-user/3433-count-mentions-per-user.java
+++ 3433-count-mentions-per-user/3433-count-mentions-per-user.java
@@ -1,46 +1,71 @@
 class Solution {
+    private static final int MOD = 105;
+    private static final int MOD1 = MOD - 1;
+    private static final int[] m = new int[200];
+    private static final int[] offline = new int[200];
+
     public int[] countMentions(int numberOfUsers, List<List<String>> events) {
-        int[] mentions = new int[numberOfUsers];
-        int[] offlineTime = new int[numberOfUsers];
-
-        events.sort((a, b) -> {
-            int timeA = Integer.parseInt(a.get(1));
-            int timeB = Integer.parseInt(b.get(1));
-            return timeA == timeB ? b.get(0).compareTo(a.get(0)) : timeA - timeB;
-        });
-
-        for (List<String> event : events) {
-            if (event.get(0).equals("MESSAGE")) {
-                handleMessage(event, mentions, offlineTime);
-            } else if (event.get(0).equals("OFFLINE")) {
-                handleOffline(event, offlineTime);
-            }
-        }
-
-        return mentions;
-    }
-
-    private void handleMessage(List<String> event, int[] mentions, int[] offlineTime) {
-        int timestamp = Integer.parseInt(event.get(1));
-        String[] tokens = event.get(2).split(" ");
-
-        for (String token : tokens) {
-            if (token.equals("ALL")) {
-                for (int i = 0; i < mentions.length; i++) mentions[i]++;
-            } else if (token.equals("HERE")) {
-                for (int i = 0; i < mentions.length; i++) {
-                    if (offlineTime[i] == 0 || offlineTime[i] + 60 <= timestamp) mentions[i]++;
+        final int[] r = new int[numberOfUsers];
+        int all = 0;
+        int mlen = 0;
+        for (var event : events) {
+            final int ts = parseInt(event.get(1));
+            final String ids = event.get(2);
+            if (event.getFirst().equals("MESSAGE")) {
+                if (ids.equals("ALL")) {
+                   all++;
+                } else if (ids.equals("HERE")) {
+                    m[mlen++] = ts * MOD + MOD1;
+                } else {
+                    for (int i = 0;;) {
+                        final int nextSpace = ids.indexOf(' ', i);
+                        if (nextSpace < 0) {
+                            r[parseInt(ids, i + 2, ids.length())]++;
+                            break;
+                        }
+                        r[parseInt(ids, i + 2, nextSpace)]++;
+                        i = nextSpace + 1;
+                    }
                 }
             } else {
-                int id = Integer.parseInt(token.substring(2));
-                mentions[id]++;
+                final int id = parseInt(ids);
+                m[mlen++] = ts * MOD + id + 1;
+                m[mlen++] = (ts + 60) * MOD;
             }
         }
+
+        int os = 0;
+        int oe = 0;
+        Arrays.sort(m, 0, mlen);
+        for (int i = 0; i < mlen; i++) {
+            final var message = m[i] % MOD;
+            if (message == 0) {
+                os++;
+            } else if (message == MOD1) {
+                all++;
+                for (int j = os; j < oe; j++) {
+                    r[offline[j]]--;
+                }
+            } else {
+                offline[oe++] = message - 1;
+            }
+        }
+        if (all > 0) {
+            for (int i = 0; i < numberOfUsers; i++) {
+                r[i] += all;
+            }
+        }
+        return r;
+    }
+    private static final int parseInt(String s) {
+        return parseInt(s, 0, s.length());
     }
 
-    private void handleOffline(List<String> event, int[] offlineTime) {
-        int timestamp = Integer.parseInt(event.get(1));
-        int id = Integer.parseInt(event.get(2));
-        offlineTime[id] = timestamp;
+    private static final int parseInt(String ids, int start, int end) {
+        int r = ids.charAt(start) - '0';
+        for (int i = start + 1; i < end; i++) {
+            r = r * 10 + ids.charAt(i) - '0';
+        }
+        return r;
     }
 }
