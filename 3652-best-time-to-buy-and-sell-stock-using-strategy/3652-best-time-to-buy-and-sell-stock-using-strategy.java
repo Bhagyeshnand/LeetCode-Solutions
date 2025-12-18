53db73bf253a0614d6425fb30eb9271e505e9c5c
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2025-12-18 08:05:45 +0530

    Time: 3 ms (100%), Space: 107 MB (16.78%) - LeetHub

--- 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.java
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.java
@@ -1,26 +1,25 @@
 class Solution {
-    private static final int MAX_SIZE = 100001;
-    private static long[] prefixSum = new long[MAX_SIZE];
-    
     public long maxProfit(int[] prices, int[] strategy, int k) {
-        int n = prices.length, half = k / 2;
-        Arrays.fill(prefixSum, 0, n + 1, 0);
-        
-        for (int i = 0; i < n; i++) {
-            prefixSum[i + 1] = prefixSum[i] + (long) strategy[i] * prices[i];
+        long sum = 0;
+        int kk = k / 2, n = prices.length;
+        long current = 0, max = 0;
+        for(int i = 0; i < kk; i++) {
+            int val = prices[i] * strategy[i];
+            sum += val;
+            current += prices[i] - val;
         }
-        
-        long windowSum = 0;
-        for (int i = half; i < k; i++) {
-            windowSum += prices[i];
+        for(int i = kk; i < k; i++) {
+            int val = prices[i] * strategy[i];
+            sum += val;
+            current += prices[i] - val - prices[i - kk];
         }
-        long maxProfit = Math.max(prefixSum[n], windowSum + prefixSum[n] - prefixSum[k]);
-        
-        for (int start = 1; start + k <= n; start++) {
-            windowSum += prices[start + k - 1] - prices[start + half - 1];
-            maxProfit = Math.max(maxProfit, windowSum + prefixSum[n] - prefixSum[start + k] + prefixSum[start]);
+        max = Math.max(max, current);
+        for(int i = k; i < n; i++) {
+            int val = prices[i] * strategy[i];
+            sum += val;
+            current += prices[i] - val - prices[i - kk] + prices[i - k] * strategy[i - k];
+            max = Math.max(max, current);
         }
-        
-        return maxProfit;
+        return sum + max;
     }
 }
