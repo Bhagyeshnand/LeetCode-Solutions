246ba81541d457a8c424801aaf8aa8567525e24b
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2026-01-03 10:48:37 +0530

    Time: 0 ms (100%), Space: 42.3 MB (35.38%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
@@ -1,15 +1,35 @@
 class Solution {
-    public int numOfWays(int n) {
-        final int MOD = 1_000_000_007;
-        long A = 6, B = 6;
-        
-        for (int i = 2; i <= n; i++) {
-            long newA = (2 * A + 2 * B) % MOD;
-            long newB = (2 * A + 3 * B) % MOD;
-            A = newA;
-            B = newB;
+    int mod = 1_000_000_007;
+    public long[][] matrixMultiply(long[][] mat1, long[][] mat2){
+        long[][] ans = new long[mat1.length][mat2[0].length];
+        for(int i=0;i<mat1.length;i++){
+            for(int j=0;j<mat2[0].length;j++){
+                for(int k=0;k<mat2.length;k++){
+                    ans[i][j] = ((ans[i][j] + mat1[i][k]*mat2[k][j]) % mod);
+                }
+            }
         }
-        
-        return (int) ((A + B) % MOD);
+
+        return ans;
+    }
+    public long[][] matrixPower(long[][] mat, int n){
+        long[][] ans = {{1, 0},{0, 1}};
+        long[][] curr = mat;
+        while(n!=0){
+            if((n&1) == 1){
+                ans = matrixMultiply(ans, curr);
+            }
+            curr = matrixMultiply(curr, curr);
+            n >>= 1;
+        }
+
+        return ans;
+    }
+    public int numOfWays(int n) {
+        long[][] mat = {{3,2},{2,2}};
+        long[][] multiplyMat = matrixPower(mat, n-1);
+        long[][] initialVector = {{6}, {6}};
+        long[][] finalVector = matrixMultiply(multiplyMat, initialVector);
+        return (int)((finalVector[0][0] + finalVector[1][0]) % mod);
     }
 }
