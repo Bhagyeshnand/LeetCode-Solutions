26bf76b34c8d02c8968ec8557a3f2e41bf8dd0a5
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2026-01-05 08:29:11 +0530

    Time: 3 ms (100%), Space: 68.2 MB (13.94%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
@@ -1,25 +1,26 @@
 class Solution {
     public long maxMatrixSum(int[][] matrix) {
-        long s=0;
-        int r=matrix.length;
-        int c=matrix[0].length;
-        int m=Integer.MAX_VALUE;
-        boolean n=false;
+        int n = matrix.length;
 
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                int v=matrix[i][j];
-                if(v<0){
-                    n=!n;
-                    v=-v;
+        int leastElement = 100001;
+        long sum = 0L;
+        int negativeCount = 0;
+
+        for(int[] rows : matrix ){
+            for(int value : rows){
+                if(value < 0){
+                    negativeCount++;
+                    value = -value;
                 }
-                s+=v;
-                if (v<m){
-                    m=v;
+
+                sum += value;
+                if(value < leastElement){
+                    leastElement = value;
                 }
+                
             }
         }
-        if(n)return s-m*2;
-        return s;
+
+        return (negativeCount%2 == 0) ? sum : (sum - 2*leastElement);
     }
 }
