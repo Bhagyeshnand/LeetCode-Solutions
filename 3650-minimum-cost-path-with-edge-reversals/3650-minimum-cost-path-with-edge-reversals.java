ede0c939875556bd5734fc5abfbfbf263551e031
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2026-01-27 21:40:09 +0530

    Time: 70 ms (93.12%), Space: 276 MB (35.22%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.java
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.java
@@ -1,45 +1,50 @@
 class Solution {
+    static class Edge {
+        int to;
+        int weight;
+        Edge(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
+    }
     public int minCost(int n, int[][] edges) {
-        // Building augmented graph
-        ArrayList<int[]>[] adj = new ArrayList[n];
-        for (int i = 0; i < n; i++) 
-            adj[i] = new ArrayList<>();
+        List<Edge>[] graph = new ArrayList[n];
 
-        for (int i = 0; i < edges.length; i++) {
-            int u = edges[i][0], v = edges[i][1], w = edges[i][2];
-            adj[u].add(new int[]{v, w});
-            adj[v].add(new int[]{u, 2 * w});
+        for(int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
         }
 
-        // Initialize distance array from 0 node
-        final int INF = 1000000000;
-        int[] dist = new int[n];
-        for (int i = 0; i < n; i++) 
-            dist[i] = INF;
+        for(int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            graph[u].add(new Edge(v, w));
+            graph[v].add(new Edge(u, 2*w));
+        }
+
+        int[] dist = new int[n]; //najkraci put do svakog cvora od pocetnog
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[0] = 0;
 
-        // Dijkstra
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        pq.add(new int[]{0, 0});
-
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int d = cur[0], u = cur[1];
-            if (u == n - 1)  // Early exit
-                return d;
-            if (d != dist[u])  // Stale edge
-                continue;
-
-            for (int i = 0; i < adj[u].size(); i++) {
-                int[] e = adj[u].get(i);
-                int v = e[0], w = e[1];
-                if (dist[u] + w < dist[v]) {  // Edge relaxation
-                    dist[v] = dist[u] + w;
-                    pq.add(new int[]{dist[v], v});
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        pq.add(new int[]{0, 0});//dodajemo cvor sa distancom do njega
+        
+        while(!pq.isEmpty()) {
+            int[] current = pq.poll();
+            int currentNode = current[0];
+            int distanceToCurrentNode = current[1];
+            //if(currentNode == n - 1) return dist[currentNode];
+            if(currentNode == n - 1) return distanceToCurrentNode;
+            for(Edge edge : graph[currentNode]) {
+                int nextNode = edge.to;
+                int weight = edge.weight;
+                if(dist[nextNode] > distanceToCurrentNode + weight) {
+                    dist[nextNode] = distanceToCurrentNode + weight;
+                    pq.add(new int[]{nextNode, dist[nextNode]});
                 }
             }
         }
-
+        //return dist[n - 1] == Integer.MAX_VALUE ? -1 : dist[n - 1] ;
         return -1;
     }
 }
