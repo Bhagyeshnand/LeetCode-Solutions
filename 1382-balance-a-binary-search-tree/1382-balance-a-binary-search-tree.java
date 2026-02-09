cdeae1c6224087c8585d5ff0d55fdd60e2433ad3
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2026-02-09 08:36:00 +0530

    Time: 2 ms (97.82%), Space: 48.1 MB (88.59%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -1,40 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    ArrayList<TreeNode> res = new ArrayList<TreeNode>();
     public TreeNode balanceBST(TreeNode root) {
-        List<Integer> sortedElements = new ArrayList<>();
-        inOrderTraversal(root, sortedElements);
-        return buildBalancedBST(sortedElements, 0, sortedElements.size() - 1);
+        inorder(root);
+        return Build_BST(0, res.size() - 1);
     }
-    
-    private void inOrderTraversal(TreeNode node, List<Integer> sortedElements) {
-        if (node == null) {
-            return;
-        }
-        inOrderTraversal(node.left, sortedElements);
-        sortedElements.add(node.val);
-        inOrderTraversal(node.right, sortedElements);
-    }
-    
-    private TreeNode buildBalancedBST(List<Integer> elements, int start, int end) {
-        if (start > end) {
-            return null;
-        }
-        int mid = start + (end - start) / 2;
-        TreeNode node = new TreeNode(elements.get(mid));
-        node.left = buildBalancedBST(elements, start, mid - 1);
-        node.right = buildBalancedBST(elements, mid + 1, end);
-        return node;
-    }
-}
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+    public void inorder(TreeNode root ){
+         if(root == null) return;
+         inorder(root.left);
+         res.add(root);
+         inorder(root.right);
+    }
+    
+    public TreeNode Build_BST( int start, int end){
+        if(start > end) return null;
+        int mid = (start + end) / 2;
+        TreeNode node = res.get(mid);
+        node.left = Build_BST( start, mid - 1);
+        node.right = Build_BST( mid + 1, end);
+        return node;
     }
 }
