ab5c7d550b83b1031e8cfb13bd6bccc09dd20149
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2026-04-14 17:29:23 +0530

    Time: 20 ms (98.39%), Space: 46.3 MB (50%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
@@ -1,71 +1,26 @@
 class Solution {
-    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
-        // Sort robots and factories by position for optimal assignment
-        Collections.sort(robot);
-        Arrays.sort(factory, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        int m = robot.size();
-        int n = factory.length; 
-        
-        // dp[i][j] represents min total distance for robots[i:] using factories[j:]
-        long[][] dp = new long[m + 1][n + 1];
-        
-        // Set last column to MAX_VALUE as boundary condition
-        for (int i = 0; i < m; i++) {
-            dp[i][n] = Long.MAX_VALUE;
-        }
-        
-        // Process each factory from right to left
-        for (int j = n - 1; j >= 0; j--) {
-            // Track cumulative distance from current factory to robots
-            long prefix = 0;
-            // Use deque to maintain potential optimal assignments
-            Deque<Pair<Integer, Long>> qq = new ArrayDeque<>();
-            // Initialize with boundary condition
-            qq.offer(new Pair<>(m, 0L));
-            
-            // Process each robot from right to left
-            for (int i = m - 1; i >= 0; i--) {
-                // Add distance from current robot to current factory
-                prefix += Math.abs(robot.get(i) - factory[j][0]);
-                
-                // Remove assignments that exceed factory capacity
-                while (!qq.isEmpty() && qq.peekFirst().getKey() > i + factory[j][1]) {
-                    qq.pollFirst();
-                }
-                
-                // Remove suboptimal assignments
-                while (!qq.isEmpty() && qq.peekLast().getValue() >= dp[i][j + 1] - prefix) {
-                    qq.pollLast();
-                }
-                
-                // Add current state to deque
-                qq.offerLast(new Pair<>(i, dp[i][j + 1] - prefix));
-                // Update dp with optimal assignment
-                dp[i][j] = qq.peekFirst().getValue() + prefix;
+    public long minimumTotalDistance(List<Integer> r, int[][] f) {
+        Collections.sort(r);
+        Arrays.sort(f, (x, y) -> x[0] - y[0]);
+        int R = r.size();
+        int F = f.length;
+        long[][] dp = new long[R + 1][F + 1];
+        for (int i = 0; i < R; i++) dp[i][F] = Long.MAX_VALUE / 4;
+        for (int j = F - 1; j >= 0; j--) {
+            long distSum = 0;
+            ArrayDeque<long[]> dq = new ArrayDeque<>();
+            dq.addLast(new long[]{R, 0});
+            for (int i = R - 1; i >= 0; i--) {
+                distSum += Math.abs(r.get(i) - f[j][0]);
+                while (!dq.isEmpty() && dq.peekFirst()[0] > i + f[j][1])
+                    dq.pollFirst();
+                long val = dp[i][j + 1] - distSum;
+                while (!dq.isEmpty() && dq.peekLast()[1] >= val)
+                    dq.pollLast();
+                dq.addLast(new long[]{i, val});
+                dp[i][j] = dq.peekFirst()[1] + distSum;
             }
         }
-        
-        // Return minimum total distance for all robots
         return dp[0][0];
-    }
-    
-    // Helper class to store key-value pairs
-    private static class Pair<K, V> {
-        private K key;
-        private V value;
-        
-        public Pair(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-        
-        public K getKey() {
-            return key;
-        }
-        
-        public V getValue() {
-            return value;
-        }
     }
 }
