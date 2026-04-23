d757bbc0f52ac719e9a34821c87dc6881c515490
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2026-04-23 15:04:07 +0530

    Time: 14 ms (100%), Space: 108 MB (95.07%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.java
+++ 2615-sum-of-distances/2615-sum-of-distances.java
@@ -1,31 +1,28 @@
-
 class Solution {
+    private class Data {
+        long s1, s2;
+        int c1, c2;
+    }
+
     public long[] distance(int[] nums) {
         int n = nums.length;
-        Map<Integer, List<Integer>> groups = new HashMap<>();
-
+        Map<Integer, Data> m = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            groups.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+            Data d = m.get(nums[i]);
+            if (d == null)
+                m.put(nums[i], d = new Data());
+            d.s2 += i;
+            d.c2 += 1;
         }
-
-        long[] res = new long[n];
-
-        for (List<Integer> group : groups.values()) {
-            long total = 0;
-            for (int idx : group) {
-                total += idx;
-            }
-
-            long prefixTotal = 0;
-            int sz = group.size();
-
-            for (int i = 0; i < sz; i++) {
-                int idx = group.get(i);
-                res[idx] = total - prefixTotal * 2 + (long) idx * (2 * i - sz);
-                prefixTotal += idx;
-            }
+        long[] r = new long[n];
+        for (int i = 0; i < n; i++) {
+            Data x = m.get(nums[i]);
+            r[i] = x.s2 - x.s1 + 1L * x.c1 * i - 1L * i * x.c2;
+            x.s1 += i;
+            x.s2 -= i;
+            x.c1 += 1;
+            x.c2 -= 1;
         }
-
-        return res;
+        return r;
     }
 }
