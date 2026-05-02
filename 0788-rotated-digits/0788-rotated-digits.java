fdabdab00227e306d83003fd78ec588f7541a692
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2026-05-02 16:45:04 +0530

    Time: 0 ms (100%), Space: 42.3 MB (44.8%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.java
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -1,33 +1,39 @@
+
 class Solution {
     public int rotatedDigits(int n) {
-        int[] dp = new int[n + 1];
-        int count = 0;
+        char[] digits = String.valueOf(n).toCharArray();
+        Integer[][][] memo = new Integer[digits.length][2][2];
+        return dfs(0, 1, 0, digits, memo);
+    }
 
-        for (int i = 0; i <= n; i++) {
-            if (i < 10) {
-                if (i == 0 || i == 1 || i == 8) {
-                    dp[i] = 1;
-                } else if (i == 2 || i == 5 || i == 6 || i == 9) {
-                    dp[i] = 2;
-                    count++;
-                } else {
-                    dp[i] = 0;
-                }
-            } else {
-                int a = dp[i / 10];
-                int b = dp[i % 10];
-
-                if (a == 1 && b == 1) {
-                    dp[i] = 1;
-                } else if (a >= 1 && b >= 1) {
-                    dp[i] = 2;
-                    count++;
-                } else {
-                    dp[i] = 0;
-                }
-            }
+    private int dfs(int pos, int tight, int changed, char[] digits, Integer[][][] memo) {
+        if (pos == digits.length) {
+            return changed == 1 ? 1 : 0;
         }
 
+        if (memo[pos][tight][changed] != null) {
+            return memo[pos][tight][changed];
+        }
+
+        int limit = tight == 1 ? digits[pos] - '0' : 9;
+        int count = 0;
+
+        for (int d = 0; d <= limit; d++) {
+            if (d == 3 || d == 4 || d == 7) {
+                continue;
+            }
+
+            int nextTight = tight == 1 && d == limit ? 1 : 0;
+            int nextChanged = changed;
+
+            if (d == 2 || d == 5 || d == 6 || d == 9) {
+                nextChanged = 1;
+            }
+
+            count += dfs(pos + 1, nextTight, nextChanged, digits, memo);
+        }
+
+        memo[pos][tight][changed] = count;
         return count;
     }
 }
