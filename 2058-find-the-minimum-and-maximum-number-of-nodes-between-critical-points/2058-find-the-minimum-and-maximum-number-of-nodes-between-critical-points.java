2bed1c366e86d2b863886bd741aeca184e8d073d
Bhagyesh Nand <117556823+Bhagyeshnand@users.noreply.github.com>
2026-08-31 17:19:15 +0530

    Time: 3 ms (100%), Space: 106.2 MB (42.07%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -11,5 +11,64 @@
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
         
+        int prev=head.val;
+        int curr,next;
+        ListNode temp=head.next;
+
+        int mindis;
+        int maxdis;
+
+        int firstidx=0,curridx=0,previdx=0;
+
+        
+
+        while(temp.next!=null){
+            curr=temp.val;
+            next=temp.next.val;
+            temp=temp.next;
+            if(curr<prev && curr<next || curr>prev && curr>next ) {
+                curridx=1;
+                firstidx=1;
+                prev=curr;
+                break;
+            }
+            prev=curr;
+        }
+
+        if(firstidx==0) return new int[]{-1,-1};
+
+        while(temp.next!=null){
+            curr=temp.val;
+            next=temp.next.val;
+            temp=temp.next;
+            curridx++;
+            if(curr<prev && curr<next || curr>prev && curr>next ) {
+                previdx=curridx;
+                prev=curr;
+                break;
+            }
+            prev=curr;
+        }
+
+        if(previdx==0) return new int[]{-1,-1};
+
+        mindis=maxdis=curridx-firstidx;
+
+           while(temp.next!=null){
+            curr=temp.val;
+            next=temp.next.val;
+            temp=temp.next;
+            curridx++;
+            if(curr<prev && curr<next || curr>prev && curr>next ) {
+                maxdis=curridx-firstidx;
+                if((curridx-previdx)<mindis) mindis=curridx-previdx;
+                previdx=curridx;
+               
+            }
+            prev=curr;
+        }
+
+        return new int[]{mindis,maxdis};
+
     }
 }
